212e40e89685cee204f48ac8cc8de3cd3caab646
Xavier Denis <xldenis@gmail.com>
2021-08-29 23:36:33 +0200

    Clean up binaries

--- creusot/src/bin/cargo-creusot.rs
+++ creusot/src/bin/cargo-creusot.rs
@@ -16,17 +16,17 @@ fn main() {
         (author: "Xavier Denis <xldenis@lri.fr>")
         (@setting TrailingVarArg)
         (@setting AllowLeadingHyphen)
-        (@arg PACKAGE: -p --package <PKG> "package to verify")
+        (@arg PACKAGE: -p --package [PKG] "package to verify")
         (@arg flags: ... "cargo flags")
     )
-    .get_matches_from(std::env::args());
+    .get_matches_from(std::env::args().skip(2));
 
     let cargo_cmd = if std::env::var_os("CREUSOT_CONTINUE").is_some() { "build" } else { "check" };
 
     let mut cmd = Command::new(cargo_path);
     cmd.arg(&cargo_cmd)
         .arg("-q")
-        .args(std::env::args().skip(1))
+        .args(std::env::args().skip(2))
         .env("RUSTC_WRAPPER", creusot_rustc_path);
 
     if let Some(tgt) = matches.value_of("pkg") {
--- creusot/src/bin/creusot-rustc.rs
+++ creusot/src/bin/creusot-rustc.rs
@@ -4,8 +4,6 @@ use std::{
     process::{exit, Command},
 };
 
-use creusot::util::sysroot_path;
-
 fn main() {
     let mut args = env::args().skip(1).collect::<Vec<_>>();
 
@@ -19,13 +17,33 @@ fn main() {
         .expect("current executable path invalid")
         .with_file_name("creusot-driver");
 
+    let sysroot = sysroot_path();
+
     let exit_code = Command::new(creusot_driver_path)
         .args(args)
-        .args(vec!["--sysroot".into(), sysroot_path()])
+        .args(vec!["--sysroot".into(), sysroot])
         .status()
         .expect("creusot-driver failed to execute");
 
     if !exit_code.success() {
         exit(exit_code.code().unwrap_or(-1));
     }
+}
+
+fn sysroot_path() -> String {
+    let toolchain: toml::Value = toml::from_str(include_str!("../../../rust-toolchain")).unwrap();
+    let channel = toolchain["toolchain"]["channel"].as_str().unwrap();
+
+    let output = Command::new("rustup")
+        .arg("run")
+        .arg(channel)
+        .arg("rustc")
+        .arg("--print")
+        .arg("sysroot")
+        .output()
+        .unwrap();
+
+    print!("{}", String::from_utf8(output.stderr).ok().unwrap());
+
+    String::from_utf8(output.stdout).unwrap().trim().to_owned()
 }
