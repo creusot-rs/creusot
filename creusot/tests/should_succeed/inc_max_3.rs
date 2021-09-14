3011ffaed9edbcacfa5c4a86b62eb68d625ea1d5
松下祐介 <y.skm24t@gmail.com>
2021-09-15 00:11:11 +0900

    fix literals in inc_max_3

--- creusot/tests/should_succeed/inc_max_3.rs
+++ creusot/tests/should_succeed/inc_max_3.rs
@@ -11,7 +11,7 @@ fn swap<'a>(mma: &'a mut &'a mut u32, mmb: &'a mut &'a mut u32) {
   *mmb = tmp;
 }
 
-#[requires(*ma <= 1000_000u32 && *mb <= 1000_000u32 && *mc <= 1000_000u32)]
+#[requires(*ma <= 1_000_000u32 && *mb <= 1_000_000u32 && *mc <= 1_000_000u32)]
 #[ensures(^ma != ^mb && ^mb != ^mc && ^mc != ^ma)]
 fn inc_max_3<'a>(ma: &'a mut u32, mb: &'a mut u32, mc: &'a mut u32) {
   if *ma < *mb {
@@ -27,7 +27,7 @@ fn inc_max_3<'a>(ma: &'a mut u32, mb: &'a mut u32, mc: &'a mut u32) {
   *mb += 1;
 }
 
-#[requires(a <= 1000_000u32 && b <= 1000_000u32 && c <= 1000_000u32)]
+#[requires(a <= 1_000_000u32 && b <= 1_000_000u32 && c <= 1_000_000u32)]
 fn test_inc_max_3(mut a: u32, mut b: u32, mut c: u32) {
   inc_max_3(&mut a, &mut b, &mut c);
   assert!(a != b && b != c && c != a);
