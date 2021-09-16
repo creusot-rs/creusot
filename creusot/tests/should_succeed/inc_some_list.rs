2300a0946e22d5fb7c17c3533f08981453a79e21
松下祐介 <y.skm24t@gmail.com>
2021-09-16 14:22:18 +0900

    Add inc_some_list

--- creusot/tests/should_succeed/inc_some_list.rs
+++ creusot/tests/should_succeed/inc_some_list.rs
@@ -10,8 +10,6 @@ fn random() -> bool {
     true
 }
 
-fn clone() {}
-
 enum List {
     Cons(u32, Box<List>),
     Nil,
@@ -19,15 +17,25 @@ enum List {
 use List::*;
 
 logic! {
-fn sum(l: List) -> Int {
+    fn sum(l: List) -> Int {
+        match l {
+            Cons(a, l2) => Int::from(a) + sum(*l2),
+            Nil => 0,
+        }
+    }
+}
+
+#[requires(sum(*l) <= 2_000_000)]
+#[ensures(Int::from(result) == sum(*l))]
+fn sumx(l: &List) -> u32 {
     match l {
-        Cons(a, l2) => Int::from(a) + sum(*l2),
+        Cons(a, l2) => *a + sumx(l2),
         Nil => 0,
     }
 }
-}
 
-#[ensures(sum(*ml) + Int::from(^result) == sum(^ml) + Int::from(*result))]
+#[ensures(sum(*ml) - sum(^ml) == Int::from(*result) - Int::from(^result))]
+#[ensures(Int::from(*result) <= sum(*ml))]
 fn take_some_list(ml: &mut List) -> &mut u32 {
     match ml {
         Cons(ma, ml2) => {
@@ -39,4 +47,12 @@ fn take_some_list(ml: &mut List) -> &mut u32 {
         }
         Nil => loop {},
     }
+}
+
+#[requires(sum(l) <= 1_000_000 && k <=1_000_000u32)]
+fn inc_some_list(mut l: List, k: u32) {
+    let sum0 = sumx(&l);
+    let ma = take_some_list(&mut l);
+    *ma += k;
+    assert!(sumx(&l) == sum0 + k);
 }
