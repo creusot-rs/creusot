56a0725a3205083037a60f9e45612a07e8c594d3
Xavier Denis <xldenis@gmail.com>
2021-10-01 15:26:22 +0200

    Move the pearlite parser to a *library* rather than fork of syn

---
+++ pearlite-syn/src/lib.rs
@@ -0,0 +1,6 @@
+#[macro_use]
+pub(crate) mod macros;
+pub(crate) mod print;
+
+pub mod term;
+pub use term::*;
---
+++ pearlite-syn/tests/test_term.rs
@@ -0,0 +1,265 @@
+#[macro_use]
+mod macros;
+
+use pearlite_syn::Term;
+use quote::quote;
+
+#[test]
+fn test_term() {
+    snapshot!(quote!(false ==> true) as Term, @r###"
+    TermImpl {
+        hyp: TermLit {
+            lit: Bool(
+                LitBool {
+                    value: false,
+                },
+            ),
+        },
+        eqeq_token: EqEq,
+        gt_token: Gt,
+        cons: TermLit {
+            lit: Bool(
+                LitBool {
+                    value: true,
+                },
+            ),
+        },
+    }
+    "###);
+
+    snapshot!(quote!(a === b) as Term, @r###"
+    TermLogEq {
+        lhs: TermPath {
+            inner: ExprPath {
+                attrs: [],
+                qself: None,
+                path: Path {
+                    leading_colon: None,
+                    segments: [
+                        PathSegment {
+                            ident: Ident(
+                                a,
+                            ),
+                            arguments: None,
+                        },
+                    ],
+                },
+            },
+        },
+        eqeq_token: EqEq,
+        eq_token: Eq,
+        rhs: TermPath {
+            inner: ExprPath {
+                attrs: [],
+                qself: None,
+                path: Path {
+                    leading_colon: None,
+                    segments: [
+                        PathSegment {
+                            ident: Ident(
+                                b,
+                            ),
+                            arguments: None,
+                        },
+                    ],
+                },
+            },
+        },
+    }
+    "###);
+}
+
+#[test]
+fn test_forall() {
+    snapshot!(quote!(forall<x : u32> true) as Term, @r###"
+    TermForall {
+        forall_token: Keyword [forall],
+        lt_token: Lt,
+        args: [
+            QuantArg {
+                ident: Ident(
+                    x,
+                ),
+                colon_token: Colon,
+                ty: Path(
+                    TypePath {
+                        qself: None,
+                        path: Path {
+                            leading_colon: None,
+                            segments: [
+                                PathSegment {
+                                    ident: Ident(
+                                        u32,
+                                    ),
+                                    arguments: None,
+                                },
+                            ],
+                        },
+                    },
+                ),
+            },
+        ],
+        gt_token: Gt,
+        term: TermLit {
+            lit: Bool(
+                LitBool {
+                    value: true,
+                },
+            ),
+        },
+    }
+    "###);
+}
+
+#[test]
+fn test_exists() {
+    snapshot!(quote!(exists<x : u32> true) as Term, @r###"
+    TermExists {
+        exists_token: Keyword [exists],
+        lt_token: Lt,
+        args: [
+            QuantArg {
+                ident: Ident(
+                    x,
+                ),
+                colon_token: Colon,
+                ty: Path(
+                    TypePath {
+                        qself: None,
+                        path: Path {
+                            leading_colon: None,
+                            segments: [
+                                PathSegment {
+                                    ident: Ident(
+                                        u32,
+                                    ),
+                                    arguments: None,
+                                },
+                            ],
+                        },
+                    },
+                ),
+            },
+        ],
+        gt_token: Gt,
+        term: TermLit {
+            lit: Bool(
+                LitBool {
+                    value: true,
+                },
+            ),
+        },
+    }
+    "###);
+}
+
+#[test]
+fn test_absurd() {
+    snapshot!(quote!(absurd) as Term, @r###"
+    TermAbsurd {
+        absurd_token: Keyword [absurd],
+    }
+    "###);
+}
+
+#[test]
+fn test_match() {
+    snapshot!(quote!(match x {
+        Some(x) => true,
+        None => false
+    }) as Term, @r###"
+    TermMatch {
+        match_token: Match,
+        expr: TermPath {
+            inner: ExprPath {
+                attrs: [],
+                qself: None,
+                path: Path {
+                    leading_colon: None,
+                    segments: [
+                        PathSegment {
+                            ident: Ident(
+                                x,
+                            ),
+                            arguments: None,
+                        },
+                    ],
+                },
+            },
+        },
+        brace_token: Brace,
+        arms: [
+            TermArm {
+                pat: TupleStruct(
+                    PatTupleStruct {
+                        attrs: [],
+                        path: Path {
+                            leading_colon: None,
+                            segments: [
+                                PathSegment {
+                                    ident: Ident(
+                                        Some,
+                                    ),
+                                    arguments: None,
+                                },
+                            ],
+                        },
+                        pat: PatTuple {
+                            attrs: [],
+                            paren_token: Paren,
+                            elems: [
+                                Ident(
+                                    PatIdent {
+                                        attrs: [],
+                                        by_ref: None,
+                                        mutability: None,
+                                        ident: Ident(
+                                            x,
+                                        ),
+                                        subpat: None,
+                                    },
+                                ),
+                            ],
+                        },
+                    },
+                ),
+                guard: None,
+                fat_arrow_token: FatArrow,
+                body: TermLit {
+                    lit: Bool(
+                        LitBool {
+                            value: true,
+                        },
+                    ),
+                },
+                comma: Some(
+                    Comma,
+                ),
+            },
+            TermArm {
+                pat: Ident(
+                    PatIdent {
+                        attrs: [],
+                        by_ref: None,
+                        mutability: None,
+                        ident: Ident(
+                            None,
+                        ),
+                        subpat: None,
+                    },
+                ),
+                guard: None,
+                fat_arrow_token: FatArrow,
+                body: TermLit {
+                    lit: Bool(
+                        LitBool {
+                            value: false,
+                        },
+                    ),
+                },
+                comma: None,
+            },
+        ],
+    }
+    "###);
+}
