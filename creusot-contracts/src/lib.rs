0df6cb89f113a9df722b0629cfb7b80ee8138e3b
Xavier Denis <xldenis@gmail.com>
2021-10-04 19:54:18 +0200

    Add WellFounded orders for variant

--- creusot-contracts/src/lib.rs
+++ creusot-contracts/src/lib.rs
@@ -14,3 +14,11 @@ pub use builtins::*;
 
 // Re-export the rand crate
 pub use rand;
+
+pub trait WellFounded {}
+
+impl WellFounded for u32 {}
+impl WellFounded for u64 {}
+impl WellFounded for i32 {}
+impl WellFounded for i64 {}
+impl<T : WellFounded> WellFounded for &T {}
