0bf77cbc709a520d71e9fc67c4e4519b07dae874
Xavier Denis <xldenis@gmail.com>
2021-10-06 21:55:08 +0200

    Fix unsoundness: actually run the borrow checker on program functions

--- creusot/tests/should_succeed/inc_max_3.rs
+++ creusot/tests/should_succeed/inc_max_3.rs
@@ -1,19 +1,18 @@
+// UNBOUNDED
 #![feature(register_tool, rustc_attrs)]
 #![register_tool(creusot)]
 
 extern crate creusot_contracts;
 use creusot_contracts::*;
 
+#[trusted]
 #[ensures(^mma === *mmb && ^mmb === *mma)]
-fn swap<'a>(mma: &'a mut &'a mut u32, mmb: &'a mut &'a mut u32) {
-    let tmp = *mma;
-    *mma = *mmb;
-    *mmb = tmp;
+fn swap<'a, 'b>(mma: &'a mut &'b mut u32, mmb: &'a mut &'b mut u32) {
+    std::mem::swap(mma, mmb);
 }
 
-#[requires(*ma <= 1_000_000u32 && *mb <= 1_000_000u32 && *mc <= 1_000_000u32)]
 #[ensures(^ma != ^mb && ^mb != ^mc && ^mc != ^ma)]
-fn inc_max_3<'a>(ma: &'a mut u32, mb: &'a mut u32, mc: &'a mut u32) {
+fn inc_max_3<'a>(mut ma: &'a mut u32, mut mb: &'a mut u32, mut mc: &'a mut u32) {
     if *ma < *mb {
         swap(&mut ma, &mut mb);
     }
@@ -27,7 +26,6 @@ fn inc_max_3<'a>(ma: &'a mut u32, mb: &'a mut u32, mc: &'a mut u32) {
     *mb += 1;
 }
 
-#[requires(a <= 1_000_000u32 && b <= 1_000_000u32 && c <= 1_000_000u32)]
 fn test_inc_max_3(mut a: u32, mut b: u32, mut c: u32) {
     inc_max_3(&mut a, &mut b, &mut c);
     assert!(a != b && b != c && c != a);
