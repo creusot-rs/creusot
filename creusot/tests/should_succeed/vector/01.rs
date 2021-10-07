466ea3cb02956e7e06e7b625d338645ad158046d
Yusuke Matsushita <y.skm24t@gmail.com>
2021-10-08 01:17:35 +0900

    Fix test vector/01

--- creusot/tests/should_succeed/vector/01.rs
+++ creusot/tests/should_succeed/vector/01.rs
@@ -28,29 +28,28 @@ impl<T> List<T> {
     }
 
     logic! {
-      fn get(self, ix: Int) -> Option<T> {
-        match self {
-          Cons(hd, tl) => {
-            if ix == 0 {
-              Some(hd)
-            } else {
-              tl.get(ix - 1)
+        fn get(self, ix: Int) -> Option<T> {
+            match self {
+                Cons(hd, tl) => {
+                    if ix == 0 {
+                        Some(hd)
+                    } else {
+                        tl.get(ix - 1)
+                    }
+                },
+                Nil => None
             }
-          },
-          Nil => None
         }
-
-      }
     }
 
     logic! {
-      fn push(self, v: T) -> Self {
-        match self {
-          // Box::new should be escaped
-          Cons(h, tl) => Cons(h, Box::new(tl.push(v))),
-          Nil => Cons(v, Box::new(Nil)),
+        fn push(self, v: T) -> Self {
+            match self {
+                // Box::new should be escaped
+                Cons(h, tl) => Cons(h, Box::new(tl.push(v))),
+                Nil => Cons(v, Box::new(Nil)),
+            }
         }
-      }
     }
 
     #[pure]
@@ -72,12 +71,12 @@ impl<T> List<T> {
 }
 
 logic! {
-  fn as_ref<'a, T>(opt: Option<&T>) -> &'a Option<T> {
-    match opt {
-      Some(r) => &Some(*r),
-      None => &None,
+    fn as_ref<'a, T>(opt: Option<&T>) -> &'a Option<T> {
+        match opt {
+            Some(r) => &Some(*r),
+            None => &None,
+        }
     }
-  }
 }
 
 struct MyVec<T>(Vec<T>);
@@ -94,80 +93,74 @@ pub struct GhostRecord<T>
 where
     T: ?Sized;
 
-impl<T> GhostRecord<T> {
-    logic! {
-        #[trusted]
-        fn model(self) -> T {
-            panic!("GhostRecord model")
-        }
-    }
+impl<T> Model for GhostRecord<T> {
+    type Model = T;
+}
 
+impl<T> GhostRecord<T> {
     #[trusted]
-    #[ensures(result.model() === *a)]
+    #[ensures(@result === *a)]
     fn record(a: &T) -> GhostRecord<T> {
         GhostRecord::<T>
     }
 }
 
-impl<T> MyVec<T> {
-    logic! {
-      #[trusted]
-      fn model(self) -> List<T> {
-        Nil // Dummy value
-      }
-    }
+impl<T> Model for MyVec<T> {
+    type Model = List<T>;
+}
 
+impl<T> MyVec<T> {
     #[trusted]
-    #[ensures(result.into() === self.model().len())]
+    #[ensures(result.into() === (@*self).len())]
     fn len(&self) -> usize {
         self.0.len()
     }
 
     #[trusted]
-    #[ensures(*as_ref(result) === self.model().get(ix.into()))]
+    #[ensures(*as_ref(result) === (@*self).get(ix.into()))]
     fn get(&self, ix: usize) -> Option<&T> {
         self.0.get(ix)
     }
 
     #[trusted]
-    #[ensures((^self).model() === (*self).model().push(v))]
+    #[ensures(@^self === (@*self).push(v))]
     fn push(&mut self, v: T) {
         self.0.push(v)
     }
 
     #[trusted]
-    #[requires(Int::from(ix) < self.model().len())]
-    #[ensures(*result === self.model().index(ix.into()))]
+    #[requires(Int::from(ix) < (@*self).len())]
+    #[ensures(*result === (@*self).index(ix.into()))]
     fn index(&self, ix: usize) -> &T {
         use std::ops::Index;
         self.0.index(ix)
     }
 
     #[trusted]
-    #[requires(Int::from(ix) < self.model().len())]
-    #[ensures(*result === (*self).model().index(ix.into()))]
-    #[ensures(^result === (^self).model().index(ix.into()))]
-    #[ensures(forall<j : Int> 0 <= j && j <= (^self).model().len() ==>
-      !(j === ix.into()) ==>
-      (^self).model().index(j) === (* self).model().index(j))]
-    #[ensures(self.model().len() === (^self).model().len())]
+    #[requires(Int::from(ix) < (@*self).len())]
+    #[ensures(*result === (@*self).index(ix.into()))]
+    #[ensures(^result === (@^self).index(ix.into()))]
+    #[ensures(forall<j : Int> 0 <= j && j <= (@^self).len() ==>
+        !(j === ix.into()) ==>
+        (@^self).index(j) === (@*self).index(j))]
+    #[ensures((@*self).len() === (@^self).len())]
     fn index_mut(&mut self, ix: usize) -> &mut T {
         use std::ops::IndexMut;
         self.0.index_mut(ix)
     }
 }
 
-#[ensures(forall<i : Int> 0 <= i && i < (^v).model().len() ==> (^v).model().index(i) === 0u32)]
-#[ensures((*v).model().len() === (^v).model().len())]
+#[ensures(forall<i : Int> 0 <= i && i < (@^v).len() ==> (@^v).index(i) === 0u32)]
+#[ensures((@*v).len() === (@^v).len())]
 fn all_zero(v: &mut MyVec<u32>) {
     let mut i = 0;
     // let old_v = v;
     let old_v: GhostRecord<&mut MyVec<u32>> = GhostRecord::record(&v);
     // let x = old_v.model();
-    #[invariant(proph_const, ^ v === ^ (old_v.model() : &mut _))]
-    // this shouldn't need to be an invariatn at all.. it's a *fact* for all prophecies but why3 struggles
-    #[invariant(in_bounds, v.model().len() ===  (old_v.model() : &mut MyVec<u32>).model().len())]
-    #[invariant(all_zero, forall<j : Int> 0 <= j && j < i.into() ==> v.model().index(j) === 0u32)]
+    #[invariant(proph_const, ^v === ^@old_v)]
+    // this shouldn't need to be an invariant at all.. it's a *fact* for all prophecies but why3 struggles
+    #[invariant(in_bounds, (@*v).len() ===  (@*@old_v).len())]
+    #[invariant(all_zero, forall<j : Int> 0 <= j && j < i.into() ==> (@*v).index(j) === 0u32)]
     while i < v.len() {
         *v.index_mut(i) = 0;
         i += 1;
