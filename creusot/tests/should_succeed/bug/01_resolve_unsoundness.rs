ff0d1faa7c34abecee0fb4dcfe08fa5cb0bab6d9
Xavier Denis <xldenis@gmail.com>
2021-10-21 20:47:39 +0200

    Fix issue where we could resolve uninitialized variables

---
+++ creusot/tests/should_succeed/bug/01_resolve_unsoundness.rs
@@ -0,0 +1,24 @@
+#![feature(register_tool, rustc_attrs)]
+#![register_tool(creusot)]
+#![feature(proc_macro_hygiene, stmt_expr_attributes)]
+#![feature(type_ascription)]
+
+extern crate creusot_contracts;
+
+use creusot_contracts::std::*;
+use creusot_contracts::*;
+
+// This program exhibited a bug where we resolve the borrow of `push`, causing
+// us to prove an invalid loop invariant
+// This program should not prove.
+#[ensures((@result).len() === @n)]
+fn make_vec_of_size(n: usize) -> Vec<bool> {
+    let mut out: Vec<bool> = Vec::new();
+    let mut i = 0;
+    #[invariant(loop_invariant, 0usize <= i && i <= n)]
+    while i <= n {
+        out.push(false);
+        i += 1
+    }
+    return out;
+}
