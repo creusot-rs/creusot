758824b616fa04a1ae6800f01c1ec78422e6c063
Claude Marche <Claude.Marche@inria.fr>
2021-11-08 14:54:47 +0100

    fix CI

--- creusot/tests/should_succeed/100doors.rs
+++ creusot/tests/should_succeed/100doors.rs
@@ -16,31 +16,31 @@ extern crate creusot_contracts;
 use creusot_contracts::{std::vec::Vec, *};
 
 fn main() {
-    let mut door_open : Vec<bool> = Vec::with_capacity(100);
-    let mut i : usize = 1;
+    let mut door_open: Vec<bool> = Vec::with_capacity(100);
+    let mut i: usize = 1;
     #[invariant(loop_bounds,1 <= @i && @i <= 101)]
     #[invariant(door_size, (@door_open).len() == @i - 1)]
     while i < 101 {
-       door_open.push(false);
-       i += 1;
+        door_open.push(false);
+        i += 1;
     }
     // alternative 1: not supported
-    //let mut door_open : Vec<bool> = vec![false;100];
+    //let mut door_open: Vec<bool> = vec![false;100];
     // alternative 2: not supported
-    //let mut door_open : Vec<bool> = Vec::new();
+    //let mut door_open: Vec<bool> = Vec::new();
     //door_open.resize(100,false);
     assert!(door_open.len() == 100);
-    let mut pass : usize = 1;
+    let mut pass: usize = 1;
     #[invariant(loop_bounds,1 <= @pass && @pass <= 101)]
     #[invariant(door_size, (@door_open).len() == 100)]
     while pass < 101 {
-        let mut door : usize = pass;
+        let mut door: usize = pass;
         #[invariant(loop_bounds,1 <= @door && @door <= 100 + @pass)]
         #[invariant(door_size, (@door_open).len() == 100)]
         while door <= 100 {
             door_open[door - 1] = !door_open[door - 1];
             door += pass;
         }
-	pass += 1;
+        pass += 1;
     }
 }
