4ef74b8f5d877623e529b74bf6b49f4ca8e136cb
Xavier Denis <xldenis@gmail.com>
2022-02-28 23:35:51 +0100

    Restore actual error message for unsupported types (#297)

--- creusot/src/translation/ty.rs
+++ creusot/src/translation/ty.rs
@@ -122,8 +122,8 @@ fn translate_ty_inner<'tcx>(
             names.import_prelude_module(PreludeModule::Prelude);
             MlT::TConstructor(QName::from_string("opaque_ptr").unwrap())
         }
-        _ => unreachable!(),
-        // _ => ctx.crash_and_error(span, &format!("unsupported type {:?}", ty)),
+        // _ => unreachable!(),
+        _ => ctx.crash_and_error(span, &format!("unsupported type {:?}", ty)),
     }
 }
 
