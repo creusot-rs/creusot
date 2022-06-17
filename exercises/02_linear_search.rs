2377084974cdcb3a031687aec7fc4e51df4292fa
Xavier Denis <xldenis@gmail.com>
2022-06-17 11:40:13 -0700

    Add some initial problems

---
+++ exercises/02_linear_search.rs
@@ -0,0 +1,19 @@
+extern crate creusot_contracts;
+use creusot_contracts::*;
+
+// Prove the following:
+// 1. If we return Some(i) it is the first index containing `tgt`
+// 2. If we return None, then there are no indices containing `tgt`
+fn search<T: Ord>(v: &Vec<T>, tgt: &T) -> Option<usize> {
+    let mut i = 0;
+
+    while i < v.len() {
+        if v[i] == t {
+            return Some(i);
+        }
+
+        i += 1
+    }
+
+    return None;
+}
