05cfac1cdd94568396dbfe9d1070fa3ea9270723
Xavier Denis <xldenis@gmail.com>
2022-07-20 15:36:45 -0700

    Update all_zero

--- creusot/tests/should_succeed/all_zero.rs
+++ creusot/tests/should_succeed/all_zero.rs
@@ -8,36 +8,38 @@ pub enum List {
 }
 use List::*;
 
-#[logic]
-fn len(l: List) -> Int {
-    match l {
-        Cons(_, ls) => 1 + len(*ls),
-        Nil => 0,
+impl List {
+    #[logic]
+    fn len(self) -> Int {
+        match self {
+            Cons(_, ls) => 1 + ls.len(),
+            Nil => 0,
+        }
+    }
+
+    #[logic]
+    fn get(self, ix: Int) -> Option<u32> {
+        match self {
+            Cons(x, ls) => match pearlite! { ix == 0 } {
+                true => Some(x),
+                false => ls.get(ix - 1),
+            },
+            Nil => None,
+        }
     }
 }
 
-#[logic]
-fn get(l: List, ix: Int) -> Option<u32> {
-    match l {
-        Cons(x, ls) => match pearlite! { ix == 0 } {
-            true => Some(x),
-            false => get(*ls, ix - 1),
-        },
-        Nil => None,
-    }
-}
-
-#[ensures(forall<i:Int> 0 <= i && i < len(*l) ==> get(^l, i) == Some(0u32))]
-#[ensures(len(*l) == len(^l))]
+#[ensures(forall<i:Int> 0 <= i && i < l.len() ==> (^l).get(i) == Some(0u32))]
+#[ensures((*l).len() == (^l).len())]
 pub fn all_zero(l: &mut List) {
     use List::*;
     let old_l = ghost! { l };
     let mut loop_l = l;
 
     #[invariant(zeroed,
-        (forall<i:Int> 0 <= i && i < len(**old_l) ==> get(^*old_l, i) == Some(0u32)) ==>
-            forall<i:Int> 0 <= i && i < len(*l) ==> get(^l, i) == Some(0u32))]
-    #[invariant(in_len, len(^*old_l) == len(**old_l) ==> len(^l) == len(*l))]
+        (forall<i:Int> 0 <= i && i < loop_l.len() ==> (^loop_l).get(i) == Some(0u32)) ==>
+            forall<i:Int> 0 <= i && i < old_l.len() ==> (^*old_l).get(i) == Some(0u32))]
+    #[invariant(in_len, (^loop_l).len() == loop_l.len() ==> (^*old_l).len() == old_l.len())]
     while let Cons(value, next) = loop_l {
         *value = 0;
         loop_l = next;
