a1a333d107d472bbb959dca890df9936fa809b61
LevitatingLion <levitatinglion@web.de>
2022-08-04 22:13:13 +0200

    Revert "Remove specs and tests that use closures"
    This reverts commit 87a2005694ce82cf8cdd7cdf0f1cfb74fb76da31.

--- creusot/tests/should_succeed/option.rs
+++ creusot/tests/should_succeed/option.rs
@@ -17,6 +17,9 @@ pub fn test_option() {
     // Test `unwrap_or`
     assert!(some.unwrap_or(2) == 1);
     assert!(none.unwrap_or(2) == 2);
+    // // Test `unwrap_or_else`
+    // assert!(some.unwrap_or_else(|| 2) == 1);
+    // assert!(none.unwrap_or_else(|| 2) == 2);
 
     // Test `as_mut`
     assert!(none.as_mut().is_none());
@@ -28,16 +31,48 @@ pub fn test_option() {
     assert!(none.as_ref().is_none());
     assert!(*some.as_ref().unwrap() == 1);
 
+    // // Test `ok_or`
+    // assert!(none.ok_or(2) == Err(2));
+    // assert!(some.ok_or(2) == Ok(1));
+    // // Test `ok_or_else`
+    // assert!(none.ok_or_else(|| 2) == Err(2));
+    // assert!(some.ok_or_else(|| 2) == Ok(1));
+
+    // // Test `map`
+    // assert!(none.map(|x| x + 1).is_none());
+    // assert!(some.map(|x| x + 1).unwrap() == 2);
+    // // Test `map_or`
+    // assert!(none.map_or(5, |x| x + 1) == 5);
+    // assert!(some.map_or(5, |x| x + 1) == 2);
+    // // Test `map_or_else`
+    // assert!(none.map_or_else(|| 5, |x| x + 1) == 5);
+    // assert!(some.map_or_else(|| 5, |x| x + 1) == 2);
+
     // Test `and`
     assert!(none.and(none).is_none());
     assert!(none.and(Some(2)).is_none());
     assert!(some.and(none).is_none());
     assert!(some.and(Some(2)).unwrap() == 2);
+    // // Test `and_then`
+    // assert!(none.and_then(|_| none).is_none());
+    // assert!(none.and_then(|x| Some(x + 1)).is_none());
+    // assert!(some.and_then(|_| none).is_none());
+    // assert!(some.and_then(|x| Some(x + 1)).unwrap() == 2);
     // Test `or`
     assert!(none.or(none).is_none());
     assert!(none.or(Some(2)).unwrap() == 2);
     assert!(some.or(none).unwrap() == 1);
     assert!(some.or(Some(2)).unwrap() == 1);
+    // // Test `or_else`
+    // assert!(none.or_else(|| none).is_none());
+    // assert!(none.or_else(|| Some(2)).unwrap() == 2);
+    // assert!(some.or_else(|| none).unwrap() == 1);
+    // assert!(some.or_else(|| Some(2)).unwrap() == 1);
+
+    // // Test `filter`
+    // assert!(none.filter(|x| *x == 1).is_none());
+    // assert!(some.filter(|x| *x == 1).unwrap() == 1);
+    // assert!(some.filter(|x| *x == 2).is_none());
 
     // Test `take`
     assert!(none.take().is_none());
