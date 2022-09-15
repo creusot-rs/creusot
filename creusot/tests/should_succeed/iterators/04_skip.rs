9467ed190bb6729e83d04cd4678676768679bda9
Jacques-Henri Jourdan <jacques-henri.jourdan@cnrs.fr>
2022-09-15 09:31:26 +0200

    Iterator: skip. Needed to refactor the specification of iterators, making completed a predicate taking two parameters.

---
+++ creusot/tests/should_succeed/iterators/04_skip.rs
@@ -0,0 +1,78 @@
+#![feature(slice_take)]
+extern crate creusot_contracts;
+
+use creusot_contracts::*;
+
+mod common;
+use common::*;
+
+pub struct Skip<I> {
+    iter: I,
+    n: usize,
+}
+
+impl<I> Skip<I> {
+    pub fn new(iter: I, n: usize) -> Skip<I> {
+        Skip { iter, n }
+    }
+}
+
+impl<I> Iterator for Skip<I>
+where
+    I: Iterator,
+{
+    type Item = I::Item;
+
+    #[predicate]
+    fn completed(self, o: Self) -> bool {
+        pearlite! {
+            @o.n == 0 &&
+            exists<s: Seq<Self::Item>, i: I>
+                s.len() <= @self.n &&
+                self.iter.produces(s, i) &&
+                i.completed(o.iter)
+        }
+    }
+
+    #[predicate]
+    fn produces(self, visited: Seq<Self::Item>, o: Self) -> bool {
+        pearlite! {
+            visited == Seq::EMPTY && self == o ||
+            @o.n == 0 &&
+            exists<s: Seq<Self::Item>>
+                s.len() == @self.n &&
+                self.iter.produces(s.concat(visited), o.iter) &&
+                (s.len() < @self.n ==> visited == Seq::EMPTY)
+        }
+    }
+
+    #[law]
+    #[ensures(a.produces(Seq::EMPTY, a))]
+    fn produces_refl(a: Self) {}
+
+    #[law]
+    #[requires(a.produces(ab, b))]
+    #[requires(b.produces(bc, c))]
+    #[ensures(a.produces(ab.concat(bc), c))]
+    fn produces_trans(a: Self, ab: Seq<Self::Item>, b: Self, bc: Seq<Self::Item>, c: Self) {}
+
+    #[ensures(match result {
+      None => (*self).completed(^self),
+      Some(v) => (*self).produces(Seq::singleton(v), ^self)
+    })]
+    fn next(&mut self) -> Option<I::Item> {
+        let old_self = ghost! { self };
+        let mut n = std::mem::take(&mut self.n);
+        #[invariant(proph_const, ^self == ^old_self.inner())]
+        #[invariant(produces, exists<s : Seq<Self::Item>> s.len() + @n == @old_self.n
+                              && old_self.iter.produces(s, self.iter))]
+        #[invariant(n_0, @(*self).n == 0)]
+        loop {
+            let r = self.iter.next();
+            if n == 0 || r.is_none() {
+                return r;
+            }
+            n -= 1
+        }
+    }
+}
