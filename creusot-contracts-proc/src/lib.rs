1ee344f920abbdee40316cefd015844530368ff5
Xavier Denis <xldenis@gmail.com>
2022-09-16 12:18:00 +0200

    Handle wildcards in closure preconditions

--- creusot-contracts-proc/src/lib.rs
+++ creusot-contracts-proc/src/lib.rs
@@ -200,9 +200,16 @@ pub fn requires(attr: TS1, tokens: TS1) -> TS1 {
         ContractItem::Closure(clos) => {
             let req_body = pretyping::encode_term(&term).unwrap();
 
-            let inputs = clos.inputs.clone();
             let clos_name = Ident::new("closure", Span::mixed_site());
+            let mut inputs = clos.inputs.clone();
 
+            inputs.iter_mut().enumerate().for_each(|(ix, p)| match p {
+                Pat::Wild(_) => {
+                    let id = Ident::new(&format!("bndr{}", ix), Span::mixed_site());
+                    *p = parse_quote! { #id };
+                }
+                _ => (),
+            });
             let mut args = inputs.clone();
             // Remove any type ascriptions
             args.iter_mut().for_each(|p| match p {
