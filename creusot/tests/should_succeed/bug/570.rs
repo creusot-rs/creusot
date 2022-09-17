4f9ecb5152fa89d3a2e38100bd7e8f7b71b68061
Greg Morenz <greg-morenz@droid.cafe>
2022-09-16 21:32:21 -0400

    Add dependency on parent type when indexing fields

---
+++ creusot/tests/should_succeed/bug/570.rs
@@ -0,0 +1,23 @@
+extern crate creusot_contracts;
+use creusot_contracts::logic;
+
+pub struct S1 {
+    pub f: i32,
+}
+
+pub struct S2 {
+    pub s1: S1,
+}
+
+pub fn test_program(s: S2) {
+    s.s1.f;
+}
+
+pub fn test_assign(mut s: S2) {
+    s.s1.f = 2;
+}
+
+#[logic]
+pub fn test_logic(s: S2) {
+    s.s1.f;
+}
