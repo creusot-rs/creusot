194a0c990e14195ff5f8c7681438e85f46ef059a
Dominik Stolz <d.stolz@tum.de>
2023-03-08 14:43:54 +0100

    Add tests for cloned, copied and enumerate iterators

---
+++ creusot/tests/should_succeed/iterators/13_cloned.rs
@@ -0,0 +1,63 @@
+#![feature(slice_take)]
+extern crate creusot_contracts;
+
+use creusot_contracts::*;
+
+mod common;
+use common::Iterator;
+
+pub struct Cloned<I> {
+    iter: I,
+}
+
+impl<'a, I, T: 'a> Iterator for Cloned<I>
+where
+    I: Iterator<Item = &'a T>,
+    T: Clone,
+{
+    type Item = T;
+
+    #[predicate]
+    fn completed(&mut self) -> bool {
+        pearlite! { self.iter.completed() }
+    }
+
+    #[predicate]
+    fn produces(self, visited: Seq<Self::Item>, o: Self) -> bool {
+        pearlite! {
+            exists<s: Seq<&'a T>> self.iter.produces(s, o.iter)
+                && visited.len() == s.len()
+                && forall<i: Int> 0 <= i && i < s.len() ==> visited[i] == *s[i]
+        }
+    }
+
+    #[predicate]
+    fn invariant(self) -> bool {
+        pearlite! {
+            self.iter.invariant()
+        }
+    }
+
+    #[law]
+    #[requires(a.invariant())]
+    #[ensures(a.produces(Seq::EMPTY, a))]
+    fn produces_refl(a: Self) {}
+
+    #[law]
+    #[requires(a.invariant())]
+    #[requires(b.invariant())]
+    #[requires(c.invariant())]
+    #[requires(a.produces(ab, b))]
+    #[requires(b.produces(bc, c))]
+    #[ensures(a.produces(ab.concat(bc), c))]
+    fn produces_trans(a: Self, ab: Seq<Self::Item>, b: Self, bc: Seq<Self::Item>, c: Self) {}
+
+    #[maintains((mut self).invariant())]
+    #[ensures(match result {
+      None => self.completed(),
+      Some(v) => (*self).produces(Seq::singleton(v), ^self)
+    })]
+    fn next(&mut self) -> Option<T> {
+        self.iter.next().cloned()
+    }
+}
