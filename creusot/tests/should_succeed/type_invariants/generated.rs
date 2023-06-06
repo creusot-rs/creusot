58f4dc3b1b59f37b56cfb9b26f5ac980733599e8
Dominik Stolz <d.stolz@tum.de>
2023-06-06 17:00:53 +0200

    Add test

---
+++ creusot/tests/should_succeed/type_invariants/generated.rs
@@ -0,0 +1,8 @@
+extern crate creusot_contracts;
+use creusot_contracts::{*, invariant};
+
+pub struct List<T>(T, Option<Box<List<T>>>);
+
+pub fn use_list(l: List<u32>) {
+    proof_assert!(invariant::inv(l))
+}
