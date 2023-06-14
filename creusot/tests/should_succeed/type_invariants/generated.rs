f41ad35ed1b62dfc3d1274b6f12bda929a07ee47
Dominik Stolz <d.stolz@tum.de>
2023-06-14 17:41:02 +0200

    Improve test

--- creusot/tests/should_succeed/type_invariants/generated.rs
+++ creusot/tests/should_succeed/type_invariants/generated.rs
@@ -1,8 +1,24 @@
+#![allow(incomplete_features)]
+#![feature(specialization)]
 extern crate creusot_contracts;
 use creusot_contracts::{invariant, *};
 
-pub struct List<T>(T, Option<Box<List<T>>>);
+pub struct Sum10(i32, i32);
 
-pub fn use_list(l: List<i32>) {
-    proof_assert!(invariant::inv(l))
+impl invariant::UserInv for Sum10 {
+    #[predicate]
+    #[open]
+    fn user_inv(self) -> bool {
+        pearlite! { self.0@ + self.1@ == 10 }
+    }
+}
+
+pub enum Foo<'a, T> {
+    A { f1: &'a mut Sum10, f2: usize },
+    B(T),
+}
+
+#[requires(invariant::inv(x))]
+pub fn use_foo<'a>(x: Foo<'a, (Foo<'a, u32>, &'a mut Sum10)>) {
+    proof_assert!(invariant::inv(x));
 }
