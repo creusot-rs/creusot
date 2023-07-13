9b2a2fea3116c8ad78d7d57386551f2facd917b7
Dominik Stolz <d.stolz@tum.de>
2023-07-13 09:16:56 +0200

    Fix iter_mut

--- creusot/tests/should_succeed/iterators/02_iter_mut.rs
+++ creusot/tests/should_succeed/iterators/02_iter_mut.rs
@@ -66,7 +66,7 @@ impl<'a, T> IterMut<'a, T> {
     }
 }
 
-#[requires(inv(v@))]
+#[requires(inv((*v)@) && inv((^v)@))]
 #[ensures(result.inner@ == v@)]
 #[ensures((^result.inner)@ == (^v)@)]
 #[ensures((^v)@.len() == v@.len())]
