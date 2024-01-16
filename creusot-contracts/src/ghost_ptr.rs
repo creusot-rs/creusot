cc80f8ef1f67d4d60da8c63e1b165491378779de
dewert99 <dewert10@gmail.com>
2024-01-16 14:11:06 -0800

    Add ZST references to GhostPtrToken based on thesis

--- creusot-contracts/src/ghost_ptr.rs
+++ creusot-contracts/src/ghost_ptr.rs
@@ -1,12 +1,26 @@
 // Inspired by https://plv.mpi-sws.org/rustbelt/ghostcell/ https://rust-unofficial.github.io/too-many-lists/fifth.html
 use crate::{logic::FMap, *};
-use ::std::marker::PhantomData;
+use ::std::{
+    marker::PhantomData,
+    ops::{Deref, DerefMut},
+};
 
 /// Models a fragment of the heap that maps the [`GhostPtr`]s it has permission to their value.
 /// At most one [`GhostToken`] has permission to each [`GhostPtr`]
 /// No [`GhostToken`] has permission to a dangling [`GhostPtr`]
 #[trusted]
-pub struct GhostPtrToken<T: ?Sized>(Ghost<FMap<GhostPtr<T>, T>>, PhantomData<T>);
+pub struct GhostPtrToken<T: ?Sized>(PhantomData<T>);
+
+/// ZST equivalent of [`&'a GhostPtrToken<T>`](GhostPtrToken)
+/// Can be created using [`GhostPtrToken::borrow`]
+#[trusted]
+#[derive(Copy, Clone)]
+pub struct GhostPtrTokenRef<'a, T: ?Sized>(PhantomData<&'a T>);
+
+/// ZST equivalent of [`&'a mut GhostPtrToken<T>`](GhostPtrToken)
+/// Can be created using [`GhostPtrToken::borrow_mut`]
+#[trusted]
+pub struct GhostPtrTokenMut<'a, T: ?Sized>(PhantomData<&'a mut T>);
 
 /// Thin wrapper over a raw pointer managed by a [`GhostPtr`]
 pub type GhostPtr<T> = *const T;
@@ -26,7 +40,7 @@ impl<T: ?Sized> GhostPtrToken<T> {
     /// Creates a new [`GhostPtr`] that has no permission
     #[ensures(result@ == FMap::empty())]
     pub fn new() -> Self {
-        GhostPtrToken(gh!(FMap::empty()), PhantomData)
+        GhostPtrToken(PhantomData)
     }
 
     #[trusted]
@@ -68,35 +82,12 @@ impl<T: ?Sized> GhostPtrToken<T> {
         unsafe { &*ptr }
     }
 
-    /// Shrinks the view of the `self` so that it's model is now new-model
-    #[trusted]
-    #[requires(_new_model.subset(self@))]
-    #[ensures(result@ == *_new_model)]
-    pub fn shrink_token_ref(&self, _new_model: Ghost<FMap<*const T, T>>) -> &GhostPtrToken<T> {
-        self
-    }
-
-    /// Mutably borrows `ptr` and shrinks `t` so that it can no longer be used to access `ptr`
-    // Safety no other token has permission to `self`
-    // `t` can no longer be used to access `ptr`
-    #[trusted]
-    #[requires((**self)@.contains(ptr))]
-    #[ensures(*result == *(**self)@.lookup_unsized(ptr))]
-    #[ensures((*^self)@ == (**self)@.remove(ptr))]
-    #[ensures((^*self)@ == (^^self)@.insert(ptr, ^result))]
-    // #[ensures(!(^^t)@.contains(ptr))]
-    // ^~ It shouldn't have been possible to add pointer to `t` while we were holding a mutable reference to the pointer
-    pub fn take_mut<'o, 'i>(self: &'o mut &'i mut GhostPtrToken<T>, ptr: *const T) -> &'i mut T {
-        unsafe { &mut *(ptr as *mut _) }
-    }
-
     /// Mutably borrows `ptr`
     #[requires(self@.contains(ptr))]
     #[ensures(*result == *(*self)@.lookup_unsized(ptr))]
     #[ensures((^self)@ == (*self)@.insert(ptr, ^result))]
     pub fn ptr_as_mut(&mut self, ptr: *const T) -> &mut T {
-        let mut t = self;
-        t.take_mut(ptr)
+        self.borrow_mut().take_mut(ptr)
     }
 
     /// Transfers ownership of `ptr` back into a `Box`
@@ -119,6 +110,21 @@ impl<T: ?Sized> GhostPtrToken<T> {
     /// Leaks memory iff the precondition fails
     #[requires(self@.is_empty())]
     pub fn drop(self) {}
+
+    /// Convert a shared reference in an equivalent ZST
+    #[trusted]
+    #[ensures(result@ == self@)]
+    pub fn borrow(&self) -> GhostPtrTokenRef<'_, T> {
+        GhostPtrTokenRef(PhantomData)
+    }
+
+    /// Convert a mutable reference in an equivalent ZST
+    #[trusted]
+    #[ensures(result.curr() == (*result)@)]
+    #[ensures(result.fin() == (^result)@)]
+    pub fn borrow_mut(&mut self) -> GhostPtrTokenMut<'_, T> {
+        GhostPtrTokenMut(PhantomData)
+    }
 }
 
 impl<T: ?Sized> GhostPtrExt<T> for GhostPtr<T> {
@@ -137,6 +143,100 @@ impl<T: ?Sized> GhostPtrExt<T> for GhostPtr<T> {
     #[open(self)]
     fn addr_logic(self) -> Int {
         absurd
+    }
+}
+
+impl<'a, T: ?Sized> ShallowModel for GhostPtrTokenRef<'a, T> {
+    type ShallowModelTy = FMap<GhostPtr<T>, T>;
+
+    #[trusted]
+    #[ghost]
+    #[open(self)]
+    fn shallow_model(self) -> Self::ShallowModelTy {
+        absurd
+    }
+}
+
+impl<'a, T: ?Sized> Deref for GhostPtrTokenRef<'a, T> {
+    type Target = GhostPtrToken<T>;
+
+    #[trusted]
+    #[ensures(result@ == self@)]
+    fn deref(&self) -> &Self::Target {
+        &GhostPtrToken(PhantomData)
+    }
+}
+
+impl<'a, T: ?Sized> GhostPtrTokenRef<'a, T> {
+    /// Shrinks the view of the `self` so that it's model is now new-model
+    #[trusted]
+    #[requires(_new_model.subset(self@))]
+    #[ensures(result@ == *_new_model)]
+    pub fn shrink_token_ref(self, _new_model: Ghost<FMap<*const T, T>>) -> Self {
+        self
+    }
+}
+
+impl<'a, T: ?Sized> GhostPtrTokenMut<'a, T> {
+    #[trusted]
+    #[ghost]
+    #[open(self)]
+    pub fn curr(self) -> FMap<GhostPtr<T>, T> {
+        absurd
+    }
+
+    #[trusted]
+    #[logic]
+    #[open(self)]
+    pub fn fin(self) -> FMap<GhostPtr<T>, T> {
+        absurd
+    }
+
+    #[ensures(self.fin() == self.cur())]
+    #[ensures(result@ == self.cur())]
+    pub fn shr(self) -> GhostPtrTokenRef<'a, T> {
+        GhostPtrTokenRef(PhantomData)
+    }
+
+    /// Mutably borrows `ptr` and shrinks `t` so that it can no longer be used to access `ptr`
+    // Safety no other token has permission to `self`
+    // `t` can no longer be used to access `ptr`
+    #[trusted]
+    #[requires((*self).curr().contains(ptr))]
+    #[ensures(*result == *(*self).curr().lookup_unsized(ptr))]
+    #[ensures((^self).curr() == (*self).curr().remove(ptr))]
+    #[ensures((*self).fin() == (^self).fin().insert(ptr, ^result))]
+    #[ensures(!(^self).fin().contains(ptr))]
+    pub fn take_mut(&mut self, ptr: *const T) -> &'a mut T {
+        unsafe { &mut *(ptr as *mut _) }
+    }
+}
+
+impl<'a, T> Deref for GhostPtrTokenMut<'a, T> {
+    type Target = GhostPtrToken<T>;
+
+    #[trusted]
+    #[ensures(result@ == self.curr())]
+    fn deref(&self) -> &Self::Target {
+        &GhostPtrToken(PhantomData)
+    }
+}
+
+impl<'a, T> DerefMut for GhostPtrTokenMut<'a, T> {
+    #[trusted]
+    #[ensures((*result)@ == (*self).curr())]
+    #[ensures((^self).curr() == (^result)@)]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        Box::leak(Box::new(GhostPtrToken(PhantomData)))
+    }
+}
+
+#[trusted]
+impl<'a, T> Resolve for GhostPtrTokenMut<'a, T> {
+    #[logic]
+    #[open]
+    fn resolve(self) -> bool {
+        self.curr() == self.fin()
     }
 }
 
