4bc4a63968f6d707c5cc76ecc9a995c0ef66613a
Nils Husung <husung@cs.uni-saarland.de>
2024-06-21 18:36:52 +0200

    Update exercises

--- exercises/04_all_zero.rs
+++ exercises/04_all_zero.rs
@@ -6,9 +6,7 @@ use creusot_contracts::*;
 pub fn all_zero(v: &mut Vec<u32>) {
     let mut i = 0;
     let old_v = snapshot! { v };
-    // Until https://gitlab.inria.fr/why3/why3/-/merge_requests/667 is merged
-    // the following invariant is needed to allow Why3 to remember prophecies dont change
-    #[invariant(proph_const, ^v == ^old_v.inner())]
+
     while i < v.len() {
         v[i] = 0;
         i += 1;
