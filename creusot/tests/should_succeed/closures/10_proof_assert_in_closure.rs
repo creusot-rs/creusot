35b8d58198067249d74acd9ff9a9544cc0408ddd
arnaudgolfouse <arnaud.golfouse@laposte.net>
2024-09-19 11:31:50 +0200

    Test proof_assert in closures

---
+++ creusot/tests/should_succeed/closures/10_proof_assert_in_closure.rs
@@ -0,0 +1,40 @@
+extern crate creusot_contracts;
+use creusot_contracts::{std::ops::*, *};
+
+pub fn immutable_capture() {
+    let x = 1;
+    (#[requires(x == 1i32)]
+    || {
+        proof_assert!(x == 1i32);
+    })();
+}
+
+pub fn mutable_capture() {
+    let mut x = 1;
+    (#[requires(x == 1i32)]
+    || {
+        proof_assert!(x == 1i32);
+        x = 2;
+        proof_assert!(x == 2i32);
+    })();
+}
+
+#[trusted]
+#[requires(f.precondition(()))]
+#[ensures(f.postcondition_once((), ()))]
+fn calls_closure<F: FnOnce() -> ()>(f: F) {
+    f();
+}
+
+pub fn captures_and_call() {
+    let mut x = 1;
+    let clos = #[requires(x == 1i32)]
+    #[ensures(x == 2i32)]
+    || {
+        proof_assert!(x == 1i32);
+        x = 2;
+        proof_assert!(x == 2i32);
+    };
+    calls_closure(clos);
+    proof_assert!(x == 2i32);
+}
