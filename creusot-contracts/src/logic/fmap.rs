e27e2d3ae699362236ed8e04f2d0705f13aa1c30
arnaudgolfouse <arnaud.golfouse@laposte.net>
2024-10-18 16:26:41 +0200

    Add types of ghost sequences and ghost maps

--- creusot-contracts/src/logic/fmap.rs
+++ creusot-contracts/src/logic/fmap.rs
@@ -3,9 +3,28 @@ use crate::{logic::Mapping, util::*, *};
 #[cfg_attr(not(creusot), allow(dead_code))]
 type PMap<K, V> = Mapping<K, Option<SizedW<V>>>;
 
-#[trusted]
+/// A Map type usable in pearlite and `ghost!` blocks.
+///
+/// # Ghost
+///
+/// Since [`std::collections::HashMap`] and [`std::collections::BTreeMap`] have finite
+/// capacity, this could cause some issues in ghost code:
+/// ```rust,creusot,compile_fail
+/// ghost! {
+///     let mut map = HashMap::new();
+///     for _ in 0..=usize::MAX as u128 + 1 {
+///         map.insert(0, 0); // cannot fail, since we are in a ghost block
+///     }
+///     proof_assert!(map.len() <= usize::MAX@); // by definition
+///     proof_assert!(map.len() > usize::MAX@); // uh-oh
+/// }
+/// ```
+///
+/// This type is designed for this use-case, with no restriction on the capacity.
+#[trusted] //opaque
 pub struct FMap<K, V: ?Sized>(std::marker::PhantomData<K>, std::marker::PhantomData<V>);
 
+/// Logical definitions
 impl<K, V: ?Sized> FMap<K, V> {
     #[trusted]
     #[logic]
@@ -104,7 +123,9 @@ impl<K, V: ?Sized> FMap<K, V> {
     #[logic]
     #[open]
     pub fn subset(self, other: Self) -> bool {
-        pearlite! {forall<k: K> self.contains(k) ==> other.get(k) == self.get(k)}
+        pearlite! {
+            forall<k: K> self.contains(k) ==> other.get(k) == self.get(k)
+        }
     }
 
     #[trusted]
@@ -126,7 +147,11 @@ impl<K, V: ?Sized> FMap<K, V> {
     #[trusted]
     #[logic]
     #[open(self)]
-    #[ensures(forall<k: K> result.get(k) == if other.contains(k) {None} else {self.get(k)})]
+    #[ensures(forall<k: K> result.get(k) == if other.contains(k) {
+        None
+    } else {
+        self.get(k)
+    })]
     pub fn subtract_keys(self, other: Self) -> Self {
         absurd
     }
@@ -136,7 +161,11 @@ impl<K, V: ?Sized> FMap<K, V> {
     #[requires(other.subset(self))]
     #[ensures(result.disjoint(other))]
     #[ensures(other.union(result).ext_eq(self))]
-    #[ensures(forall<k: K> result.get(k) == if other.contains(k) {None} else {self.get(k)})]
+    #[ensures(forall<k: K> result.get(k) == if other.contains(k) {
+        None
+    } else {
+        self.get(k)
+    })]
     pub fn subtract(self, other: Self) -> Self {
         self.subtract_keys(other)
     }
@@ -147,5 +176,216 @@ impl<K, V: ?Sized> FMap<K, V> {
     #[ensures((forall<k: K> self.get(k) == other.get(k)) ==> result)]
     pub fn ext_eq(self, other: Self) -> bool {
         self.view() == other.view()
+    }
+}
+
+/// Ghost definitions
+impl<K, V: ?Sized> FMap<K, V> {
+    /// Create a new, empty map on the ghost heap.
+    #[trusted]
+    #[pure]
+    #[ensures(result.is_empty())]
+    #[allow(unreachable_code)]
+    pub fn new() -> GhostBox<Self> {
+        ghost!(loop {})
+    }
+
+    /// Returns the number of elements in the map.
+    ///
+    /// If you need to get the len in pearlite, consider using [`len`](Self::len).
+    ///
+    /// # Example
+    /// ```rust,creusot
+    /// use creusot_contracts::{logic::FMap, *};
+    ///
+    /// let mut map = FMap::new();
+    /// ghost! {
+    ///     let len1 = map.len_ghost();
+    ///     map.insert(1, 21);
+    ///     map.insert(1, 42);
+    ///     map.insert(2, 50);
+    ///     let len2 = map.len_ghost();
+    ///     proof_assert!(len1 == 0);
+    ///     proof_assert!(len2 == 2);
+    /// };
+    /// ```
+    #[trusted]
+    #[pure]
+    #[ensures(result == self.len())]
+    pub fn len_ghost(&self) -> Int {
+        panic!()
+    }
+
+    /// Returns true if the map contains a value for the specified key.
+    ///
+    /// # Example
+    /// ```rust,creusot
+    /// use creusot_contracts::{logic::FMap, *};
+    ///
+    /// let mut map = FMap::new();
+    /// ghost! {
+    ///     map.insert(1, 42);
+    ///     let (b1, b2) = (map.contains_ghost(&1), map.contains_ghost(&2))
+    ///     proof_assert!(b1);
+    ///     proof_assert!(!b2);
+    /// };
+    /// ```
+    #[pure]
+    #[ensures(self.contains(*key))]
+    pub fn contains_ghost(&self, key: &K) -> bool {
+        self.get_ghost(key).is_some()
+    }
+
+    /// Returns a reference to the value corresponding to the key.
+    ///
+    /// # Example
+    /// ```rust,creusot
+    /// use creusot_contracts::{logic::FMap, *};
+    ///
+    /// let mut map = FMap::new();
+    /// ghost! {
+    ///     map.insert_ghost(1, 2);
+    ///     let x1 = map.get_ghost(&1);
+    ///     let x2 = map.get_ghost(&2);
+    ///     proof_assert!(x1 == Some(&2));
+    ///     proof_assert!(x2 == None);
+    /// };
+    /// ```
+    #[trusted]
+    #[pure]
+    #[ensures(if self.contains(*key) {
+            match result {
+                None => false,
+                Some(r) => *self.lookup_unsized(*key) == *r,
+            }
+        } else {
+            result == None
+        })]
+    pub fn get_ghost(&self, key: &K) -> Option<&V> {
+        let _ = key;
+        panic!()
+    }
+
+    /// Returns a mutable reference to the value corresponding to the key.
+    ///
+    /// # Example
+    /// ```rust,creusot
+    /// use creusot_contracts::{logic::FMap, *};
+    ///
+    /// let mut map = FMap::new();
+    /// ghost! {
+    ///     map.insert(1, 21);
+    ///     if let Some(x) = map.get_mut(&1) {
+    ///         *x = 42;
+    ///     }
+    ///     proof_assert!(map.lookup(1i32) == 42i32);
+    /// };
+    /// ```
+    #[trusted]
+    #[pure]
+    #[ensures(if self.contains(*key) {
+            match result {
+                None => false,
+                Some(r) => *(*self).lookup_unsized(*key) == *r &&
+                           *(^self).lookup_unsized(*key) == ^r,
+            }
+        } else {
+            result == None
+        })]
+    #[ensures(forall<k: K> k != *key ==> (*self).get(k) == (^self).get(k))]
+    #[ensures((*self).len() == (^self).len())]
+    pub fn get_mut(&mut self, key: &K) -> Option<&mut V> {
+        let _ = key;
+        panic!()
+    }
+
+    /// Inserts a key-value pair into the map.
+    ///
+    /// If the map did not have this key present, `None` is returned.
+    ///
+    /// # Example
+    /// ```rust,creusot
+    /// use creusot_contracts::{logic::FMap, *};
+    ///
+    /// let mut map = FMap::new();
+    /// ghost! {
+    ///     proof_assert!(map.insert(37, 41) == None);
+    ///     proof_assert!(map.is_empty() == false);
+    ///
+    ///     map.insert(37, 42);
+    ///     proof_assert!(map.insert(37, 43) == Some(42));
+    ///     proof_assert!(map.get(&37) == Some(&43));
+    /// };
+    /// ```
+    #[trusted]
+    #[pure]
+    #[ensures(^self == self.insert(key, value))]
+    #[ensures(if self.contains(key) {
+            result == Some(self.lookup(key))
+        } else {
+            result == None
+        })]
+    pub fn insert_ghost(&mut self, key: K, value: V) -> Option<V>
+    where
+        V: Sized,
+    {
+        let _ = key;
+        let _ = value;
+        panic!()
+    }
+
+    /// Same as [`Self::insert_ghost`], but for unsized values.
+    #[trusted]
+    #[pure]
+    #[ensures(^self == self.insert(key, *value))]
+    #[ensures(if self.contains(key) {
+            result == Some(self.lookup_unsized(key))
+        } else {
+            result == None
+        })]
+    pub fn insert_unsized(&mut self, key: K, value: Box<V>) -> Option<Box<V>> {
+        let _ = key;
+        let _ = value;
+        panic!()
+    }
+
+    /// Removes a key from the map, returning the value at the key if the key was previously in the map.
+    ///
+    /// # Example
+    /// ```rust,creusot
+    /// use creusot_contracts::{logic::FMap, *};
+    ///
+    /// let mut map = FMap::new();
+    /// let res = ghost! {
+    ///     map.insert(1, 42);
+    ///     let res1 = map.remove(&1);
+    ///     let res2 = map.remove(&1);
+    ///     proof_assert!(res1 == Some(42i32));
+    ///     proof_assert!(res2 == None);
+    /// };
+    /// ```
+    #[trusted]
+    #[pure]
+    #[ensures(^self == self.remove(*key))]
+    #[ensures(match self.get(*key) {
+            None => result == None,
+            Some(v) => result == Some(*v),
+        })]
+    pub fn remove_ghost(&mut self, key: &K) -> Option<V>
+    where
+        V: Sized,
+    {
+        let _ = key;
+        panic!()
+    }
+
+    /// Same as [`Self::remove_ghost`], but for unsized values.
+    #[trusted]
+    #[pure]
+    #[ensures(^self == self.remove(*key))]
+    #[ensures(self.get(*key) == None)]
+    pub fn remove_unsized(&mut self, key: &K) -> Option<Box<V>> {
+        let _ = key;
+        panic!()
     }
 }
