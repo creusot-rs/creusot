2433a4203e15c7476c5c6fe516755bf35922afb8
arnaudgolfouse <arnaud.golfouse@laposte.net>
2024-11-12 15:05:50 +0100

    Add tests

---
+++ creusot/tests/should_succeed/termination/default_impl.rs
@@ -0,0 +1,28 @@
+#![allow(incomplete_features)]
+#![feature(specialization)]
+
+extern crate creusot_contracts;
+use creusot_contracts::*;
+
+pub trait Foo {
+    #[logic]
+    fn f();
+    #[logic]
+    fn g();
+}
+default impl<T> Foo for T {
+    #[logic]
+    #[open]
+    fn f() {}
+    #[logic]
+    #[open]
+    fn g() {}
+}
+
+impl Foo for () {
+    #[logic]
+    #[open]
+    fn g() {
+        Self::f();
+    }
+}
