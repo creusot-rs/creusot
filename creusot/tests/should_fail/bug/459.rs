b3a67b2b3e1ae7d8460f1d7a36b943a95b07a6be
Jacques-Henri Jourdan <jacques-henri.jourdan@cnrs.fr>
2024-11-18 17:28:11 +0100

    Add test for #459.

---
+++ creusot/tests/should_fail/bug/459.rs
@@ -0,0 +1,19 @@
+extern crate creusot_contracts;
+use creusot_contracts::*;
+
+enum A {
+    Cons(Box<A>),
+    Nil,
+}
+
+impl DeepModel for A {
+    type DeepModelTy = Int;
+    #[logic]
+    #[open]
+    fn deep_model(self) -> Self::DeepModelTy {
+        match self {
+            A::Cons(a) => *a.deep_model() + 1,
+            A::Nil => 0,
+        }
+    }
+}
