915d69bef59391d138c7788666948e19014a5bab
arnaudgolfouse <arnaud.golfouse@laposte.net>
2024-11-28 23:37:08 +0100

    Document API of `Int`

--- creusot-contracts/src/logic/int.rs
+++ creusot-contracts/src/logic/int.rs
@@ -3,6 +3,23 @@ use crate::{
     *,
 };
 
+/// An unbounded, mathematical integer.
+///
+/// This type cannot be only be constructed in logical or ghost code.
+///
+/// # Integers in pearlite
+///
+/// Note that in pearlite, all integer literals are of type `Int`:
+/// ```
+/// # use creusot_contracts::*;
+/// let x = 1i32;
+/// //             ↓ need to use the view operator to convert `i32` to `Int`
+/// proof_assert!(x@ == 1);
+/// ```
+///
+/// You can use the usual operators on integers: `+`, `-`, `*`, `/` and `%`.
+///
+/// Note that those operators are _not_ available in ghost code.
 #[trusted]
 #[cfg_attr(
     creusot,
@@ -23,6 +40,20 @@ impl crate::Clone for Int {
 impl Copy for Int {}
 
 impl Int {
+    /// Create a new `Int` value
+    ///
+    /// The result is wrapped in a [`GhostBox`], so that it can only be access inside of
+    /// a [`ghost!`] block.
+    ///
+    /// You should not have to use this method directly: instead, use the `int` suffix
+    /// inside of a `ghost` block:
+    /// ```
+    /// # use creusot_contracts::*;
+    /// let x: GhostBox<Int> = ghost!(1int);
+    /// ghost! {
+    ///     let y: Int = 2int;
+    /// };
+    /// ```
     #[trusted]
     #[pure]
     #[ensures(*result == value@)]
@@ -32,41 +63,96 @@ impl Int {
         ghost!(panic!())
     }
 
+    /// Compute `self^p`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use creusot_contracts::*;
+    /// proof_assert!(2.pow(3) == 8);
+    /// ```
     #[trusted]
     #[logic]
     #[creusot::builtins = "int.Power.power"]
-    pub fn pow(self, _: Int) -> Int {
+    pub fn pow(self, p: Int) -> Int {
+        let _ = p;
         dead
     }
 
+    /// Compute the maximum of `self` and `x`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use creusot_contracts::*;
+    /// proof_assert!(10.max(2) == 10);
+    /// ```
     #[trusted]
     #[logic]
     #[creusot::builtins = "int.MinMax.max"]
-    pub fn max(self, _: Int) -> Int {
+    pub fn max(self, x: Int) -> Int {
+        let _ = x;
         dead
     }
 
-    #[trusted]
+    /// Compute the minimum of `self` and `x`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use creusot_contracts::*;
+    /// proof_assert!(10.max(2) == 2);
+    /// ```
     #[logic]
     #[creusot::builtins = "int.MinMax.min"]
-    pub fn min(self, _: Int) -> Int {
+    #[trusted]
+    pub fn min(self, x: Int) -> Int {
+        let _ = x;
         dead
     }
 
+    /// Compute the euclidian division of `self` by `d`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use creusot_contracts::*;
+    /// proof_assert!(10.div_euclid(3) == 3);
+    /// ```
     #[trusted]
     #[logic]
     #[creusot::builtins = "int.EuclideanDivision.div"]
-    pub fn div_euclid(self, _: Int) -> Int {
+    pub fn div_euclid(self, d: Int) -> Int {
+        let _ = d;
         dead
     }
 
+    /// Compute the remainder of the euclidian division of `self` by `d`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use creusot_contracts::*;
+    ///  proof_assert!(10.rem_euclid(3) == 1);
+    /// ```
     #[trusted]
     #[logic]
     #[creusot::builtins = "int.EuclideanDivision.mod"]
-    pub fn rem_euclid(self, _: Int) -> Int {
+    pub fn rem_euclid(self, d: Int) -> Int {
+        let _ = d;
         dead
     }
 
+    /// Compute the absolute difference of `self` and `x`.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// # use creusot_contracts::*;
+    /// proof_assert!(10.abs_diff(3) == 7);
+    /// proof_assert!(3.abs_diff(10) == 7);
+    /// proof_assert!((-5).abs_diff(5) == 10);
+    /// ```
     #[logic]
     #[open]
     pub fn abs_diff(self, other: Int) -> Int {
