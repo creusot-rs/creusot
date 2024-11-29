7937d8864c763a48c0954710f28d9bbed84ebf38
arnaudgolfouse <arnaud.golfouse@laposte.net>
2024-11-29 14:29:44 +0100

    Improve doc of the `ghost` module

--- creusot-contracts/src/ghost.rs
+++ creusot-contracts/src/ghost.rs
@@ -1,3 +1,20 @@
+//! Definitions for ghost code
+//!
+//! Ghost code is code that will be erased during the normal compilation of the program.
+//! To use ghost code in creusot, you must use the [`ghost!`] macro:
+//!
+//! ```
+//! # use creusot_contracts::*;
+//! let x: GhostBox<i32> = ghost!(1);
+//! ghost! {
+//!     let y: i32 = *x;
+//!     assert!(y == 1);
+//! };
+//! ```
+//!
+//! There are restrictions on the values that can enter/exit a `ghost!` block: see
+//! [`GhostBox`] and [`ghost!`] for more details.
+
 #[cfg(creusot)]
 use crate::resolve::structural_resolve;
 use crate::{
