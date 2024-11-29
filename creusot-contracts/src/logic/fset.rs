344bc6dc76fc43510a51e4549320b47376f62cd3
arnaudgolfouse <arnaud.golfouse@laposte.net>
2024-11-29 10:47:33 +0100

    Document logical functions of `FSet`

--- creusot-contracts/src/logic/fset.rs
+++ creusot-contracts/src/logic/fset.rs
@@ -23,6 +23,7 @@ use crate::*;
 pub struct FSet<T: ?Sized>(std::marker::PhantomData<T>);
 
 impl<T: ?Sized> FSet<T> {
+    /// The empty set.
     #[cfg(creusot)]
     #[trusted]
     #[creusot::builtins = "set.Fset.empty"]
@@ -35,6 +36,7 @@ impl<T: ?Sized> FSet<T> {
         Self::EMPTY
     }
 
+    /// Returns `true` if `e` is in the set.
     #[open]
     #[predicate]
     #[why3::attr = "inline:trivial"]
@@ -42,6 +44,9 @@ impl<T: ?Sized> FSet<T> {
         Self::mem(e, self)
     }
 
+    /// [`Self::contains`], but with the order of arguments flipped.
+    ///
+    /// This is how the function is defined in why3.
     #[doc(hidden)]
     #[trusted]
     #[logic]
@@ -50,6 +55,7 @@ impl<T: ?Sized> FSet<T> {
         dead
     }
 
+    /// Returns a new set, where `e` has been added if it was not present.
     #[open]
     #[logic]
     #[why3::attr = "inline:trivial"]
@@ -57,6 +63,9 @@ impl<T: ?Sized> FSet<T> {
         Self::add(e, self)
     }
 
+    /// [`Self::insert`], but with the order of arguments flipped.
+    ///
+    /// This is how the function is defined in why3.
     #[doc(hidden)]
     #[trusted]
     #[logic]
@@ -65,6 +74,7 @@ impl<T: ?Sized> FSet<T> {
         dead
     }
 
+    /// Returns `true` if the set contains no elements.
     #[trusted]
     #[predicate]
     #[creusot::builtins = "set.Fset.is_empty"]
@@ -72,13 +82,17 @@ impl<T: ?Sized> FSet<T> {
         dead
     }
 
+    /// Returns a new set, where `e` is not longer present.
     #[open]
     #[logic]
     #[why3::attr = "inline:trivial"]
-    pub fn remove(self, a: T) -> Self {
-        Self::rem(a, self)
+    pub fn remove(self, e: T) -> Self {
+        Self::rem(e, self)
     }
 
+    /// [`Self::remove`], but with the order of arguments flipped.
+    ///
+    /// This is how the function is defined in why3.
     #[doc(hidden)]
     #[trusted]
     #[logic]
@@ -87,20 +101,27 @@ impl<T: ?Sized> FSet<T> {
         dead
     }
 
+    /// Returns a new set, which is the union of `self` and `other`.
+    ///
+    /// An element is in the result if it is in `self` _or_ if it is in `other`.
     #[trusted]
     #[logic]
     #[creusot::builtins = "set.Fset.union"]
-    pub fn union(self, _: Self) -> Self {
+    pub fn union(self, other: Self) -> Self {
+        let _ = other;
         dead
     }
 
+    /// Returns `true` if every element of `self` is in `other`.
     #[trusted]
     #[predicate]
     #[creusot::builtins = "set.Fset.subset"]
-    pub fn is_subset(self, _: Self) -> bool {
+    pub fn is_subset(self, other: Self) -> bool {
+        let _ = other;
         dead
     }
 
+    /// Returns `true` if every element of `other` is in `self`.
     #[open]
     #[predicate]
     #[why3::attr = "inline:trivial"]
@@ -108,6 +129,7 @@ impl<T: ?Sized> FSet<T> {
         Self::is_subset(other, self)
     }
 
+    /// Returns the number of elements in the set, also called its length.
     #[trusted]
     #[logic]
     #[creusot::builtins = "set.Fset.cardinal"]
@@ -115,6 +137,12 @@ impl<T: ?Sized> FSet<T> {
         dead
     }
 
+    /// Get an arbitrary element of the set.
+    ///
+    /// # Returns
+    ///
+    /// - If the set is nonempty, the result is guaranteed to be in the set
+    /// - If the set is empty, the result is unspecified
     #[trusted]
     #[logic]
     #[creusot::builtins = "set.Fset.pick"]
@@ -125,6 +153,11 @@ impl<T: ?Sized> FSet<T> {
         dead
     }
 
+    /// Extensional equality
+    ///
+    /// Returns `true` if `self` and `other` contain exactly the same elements.
+    ///
+    /// This is in fact equivalent with normal equality.
     // FIXME: remove `trusted`
     #[trusted]
     #[open]
