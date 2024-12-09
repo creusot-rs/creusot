26f39bee71520bfbfcbfc69fea90d60081369a4c
arnaudgolfouse <arnaud.golfouse@laposte.net>
2024-12-09 18:37:25 +0100

    Add a test for PtrOwn disjointness

---
+++ creusot/tests/should_succeed/ghost/disjoint_raw_ptr.rs
@@ -0,0 +1,13 @@
+extern crate creusot_contracts;
+use creusot_contracts::{ptr_own::PtrOwn, *};
+
+pub fn foo() {
+    let (p1, mut own1) = PtrOwn::new(1i32);
+    let (p2, own2) = PtrOwn::new(1i32);
+
+    ghost! {
+        let _ = PtrOwn::disjoint_lemma(*own1.borrow_mut(), *own2.borrow());
+    };
+    proof_assert!(own1 != own2);
+    proof_assert!(p1 != p2);
+}
