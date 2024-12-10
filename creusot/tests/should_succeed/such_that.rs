0b2ccdde6523078c6d897b1def4c42f006e402e4
arnaudgolfouse <arnaud.golfouse@laposte.net>
2024-12-10 15:08:03 +0100

    Update tests

--- creusot/tests/should_succeed/such_that.rs
+++ creusot/tests/should_succeed/such_that.rs
@@ -10,7 +10,7 @@ pub fn foo() {
     let mapping = snapshot!(|x: Int| x + y@);
     let predicate = snapshot!(|x: Int| mapping[x] + 1 == 0);
     proof_assert!(predicate[-y@ - 1]);
-    let x = snapshot!(such_that(predicate));
+    let x = snapshot!(such_that(*predicate));
     proof_assert!(*x + y@ + 1 == 0);
 }
 
