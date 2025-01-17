5e132fa21c3ad71d75b165f79df483d96a5e2ae5
arnaudgolfouse <arnaud.golfouse@laposte.net>
2025-01-17 18:55:12 +0100

    Print a list of running tests at the bottom of the screen

--- creusot/tests/ui.rs
+++ creusot/tests/ui.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use libc::{c_ushort, ioctl, STDOUT_FILENO, TIOCGWINSZ};
 use std::{
     env,
     fs::File,
@@ -15,6 +16,16 @@ use termcolor::*;
 
 mod diff;
 use diff::{differ, normalize_file_path};
+
+/// Used to query the size of the terminal
+#[derive(Default)]
+#[repr(C)]
+struct TermSize {
+    row: c_ushort,
+    col: c_ushort,
+    x: c_ushort,
+    y: c_ushort,
+}
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -188,20 +199,53 @@ where
     B: Fn(&Path) -> Option<std::process::Command> + Send + Sync,
 {
     let is_tty = std::io::stdout().is_terminal();
-    let out = Mutex::new(StandardStream::stdout(if args.force_color || is_tty {
+    let out = StandardStream::stdout(if args.force_color || is_tty {
         ColorChoice::Always
     } else {
         ColorChoice::Never
-    }));
+    });
 
     let test_count = AtomicUsize::new(0);
     let test_failures = AtomicUsize::new(0);
 
     let entries = Mutex::new(glob::glob(s).expect("Failed to read glob pattern"));
     let nb_threads = thread::available_parallelism().map(|n| n.into()).unwrap_or(1usize);
+    let out = Mutex::new((Vec::new(), out));
+
+    // Print all test currently running
+    let write_in_flight = |in_flight: &Vec<String>, out: &mut StandardStream| {
+        // get terminal width
+        let mut size: TermSize = TermSize::default();
+        unsafe { ioctl(STDOUT_FILENO, TIOCGWINSZ.into(), &mut size as *mut _) };
+        let width = size.col as usize;
+        // Save cursor position
+        write!(out, "\x1b7").unwrap();
+        let mut wrote = 0;
+        write!(out, "Testing: ").unwrap();
+        wrote += "Testing: ".len();
+        for (i, name) in (&*in_flight).iter().enumerate() {
+            if i != 0 {
+                write!(out, ", ").unwrap();
+                wrote += ", ".len();
+            }
+            if wrote + name.len() + 5 > width {
+                // Do not overflow the line, or output breaks!
+                write!(out, "...").unwrap();
+                break;
+            }
+            write!(out, "{name}").unwrap();
+            wrote += name.len();
+        }
+        // restore cursor position (put it back at the beginning of the line)
+        write!(out, "\x1b8").unwrap();
+        out.flush().unwrap();
+    };
+    // erase after the cursor to the end of the screen
+    let erase_in_flight = |out: &mut StandardStream| write!(out, "\x1b[J").unwrap();
 
     thread::scope(|s| {
         let worker = || {
+            // invariant: the cursor is always at the start of the line where we should write `Testing ...`
             loop {
                 let Some(entry) = entries.lock().unwrap().next() else {
                     return;
@@ -214,9 +258,20 @@ where
                         continue;
                     }
                 }
+
+                let entry_name = entry.file_stem().unwrap().to_str().unwrap();
+
                 let output = match command_builder(&entry) {
                     None => continue,
                     Some(mut c) => {
+                        if !args.quiet {
+                            let mut out = out.lock().unwrap();
+                            let (ref mut in_flight, ref mut out) = &mut *out;
+                            in_flight.push(entry_name.to_string());
+                            erase_in_flight(out);
+                            write_in_flight(in_flight, out);
+                        }
+
                         let mut o = c.output().unwrap();
                         // Replace global paths in stderr with (a simulacrum of) local paths
                         erase_global_paths(&mut o.stderr);
@@ -230,29 +285,21 @@ where
                 let (success, buf) =
                     differ(output.clone(), &stdout, Some(&stderr), should_succeed, is_tty).unwrap();
 
-                // Default (not `quiet`): print "Testing tests/current/test ... " and flush. This happens after running the test to avoid messing up the output.
-                // if `quiet` enabled: postpone printing, store the message in `current`, only print it if the test case fails
-                let mut current: &str = &format!("Test {}: ", entry.display());
                 let mut out = out.lock().unwrap();
+                let (ref mut in_flight, ref mut out) = &mut *out;
+
                 if !args.quiet {
-                    write!(out, "{}", current).unwrap();
-                    current = "";
-                    out.flush().unwrap();
+                    if let Some(i) = in_flight.iter().position(|n| n == entry_name) {
+                        in_flight.remove(i);
+                    }
                 }
 
                 if args.bless {
-                    if success {
-                        if is_tty {
-                            // Move to beginning of line and clear line.
-                            write!(out, "\x1b[G\x1b[2K").unwrap();
-                        } else {
-                            out.set_color(ColorSpec::new().set_fg(Some(Color::Green))).unwrap();
-                            writeln!(out, "ok").unwrap();
-                        }
-                    } else {
-                        write!(out, "{current}").unwrap();
+                    if !success {
+                        erase_in_flight(out);
+                        write!(out, "{}: ", entry.display()).unwrap();
                         out.set_color(ColorSpec::new().set_fg(Some(Color::Blue))).unwrap();
-                        writeln!(&mut out, "blessed").unwrap();
+                        writeln!(out, "blessed").unwrap();
                         out.reset().unwrap();
                     }
 
@@ -268,27 +315,25 @@ where
                         std::fs::write(stderr, &output.stderr).unwrap();
                     }
                 } else {
-                    if success {
-                        if !args.quiet {
-                            if is_tty {
-                                // Move to beginning of line and clear line.
-                                write!(out, "\x1b[G\x1b[2K").unwrap();
-                            } else {
-                                out.set_color(ColorSpec::new().set_fg(Some(Color::Green))).unwrap();
-                                writeln!(out, "ok").unwrap();
-                            }
-                        }
-                    } else {
-                        write!(out, "{current}").unwrap();
+                    if !success {
+                        erase_in_flight(out);
+                        write!(out, "{}: ", entry.display()).unwrap();
                         out.set_color(ColorSpec::new().set_fg(Some(Color::Red))).unwrap();
-                        writeln!(&mut out, "failure").unwrap();
+                        writeln!(out, "failure").unwrap();
 
                         test_failures.fetch_add(1, atomic::Ordering::SeqCst);
                     };
                     out.reset().unwrap();
+                    out.flush().unwrap();
 
                     let wrt = BufferWriter::stdout(ColorChoice::Always);
                     wrt.print(&buf).unwrap();
+                }
+                if !args.quiet {
+                    erase_in_flight(out);
+                    if !in_flight.is_empty() {
+                        write_in_flight(in_flight, out);
+                    }
                 }
             }
         };
@@ -300,7 +345,7 @@ where
 
     let test_count = test_count.load(atomic::Ordering::SeqCst);
     let test_failures = test_failures.load(atomic::Ordering::SeqCst);
-    let mut out = out.into_inner().unwrap();
+    let (_, mut out) = out.into_inner().unwrap();
 
     if test_failures > 0 {
         out.set_color(ColorSpec::new().set_fg(Some(Color::Red))).unwrap();
