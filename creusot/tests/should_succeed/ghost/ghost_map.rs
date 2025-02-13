9709c5ad84669ac7591905323cd10743b2af8ff4
arnaudgolfouse <arnaud.golfouse@laposte.net>
2025-02-13 16:47:47 +0100

    Add tests

--- creusot/tests/should_succeed/ghost/ghost_map.rs
+++ creusot/tests/should_succeed/ghost/ghost_map.rs
@@ -9,11 +9,10 @@ pub fn ghost_map() {
         let length1 = map.len_ghost();
         proof_assert!(map.lookup(1i32) == 21i32);
         proof_assert!(length1 == 1);
-        let (x1, x2, x3) = (1, 2, 3); // HACK: work around an issue with promoted
-        if let Some(x) = map.get_mut_ghost(&x1) {
-            *x = 42;
+        if let Some(x) = map.get_mut_ghost(&1) {
+            *x = 43;
         }
-        proof_assert!(map.lookup(1i32) == 42i32);
+        proof_assert!(map.lookup(1i32) == 43i32);
 
         let inserted_none = map.insert_ghost(2, 50);
         let inserted_some = map.insert_ghost(2, 100);
@@ -22,26 +21,34 @@ pub fn ghost_map() {
         proof_assert!(inserted_some == Some(50i32));
         proof_assert!(length2 == 2);
         proof_assert!(map.lookup(2i32) == 100i32);
-        proof_assert!(map.lookup(1i32) == 42i32);
+        proof_assert!(map.lookup(1i32) == 43i32);
 
-        let remove_none1 = map.remove_ghost(&x3);
-        let remove_some = map.remove_ghost(&x2);
-        let remove_none2 = map.remove_ghost(&x2);
+        if let (Some(x), map2) = map.split_mut_ghost(&1) {
+            *x = 42;
+            map2.insert_ghost(2, 200);
+            map2.insert_ghost(1, 56);
+        }
+        proof_assert!(map.lookup(1i32) == 42i32);
+        proof_assert!(map.lookup(2i32) == 200i32);
+
+        let remove_none1 = map.remove_ghost(&3);
+        let remove_some = map.remove_ghost(&2);
+        let remove_none2 = map.remove_ghost(&2);
         proof_assert!(remove_none1 == None);
-        proof_assert!(remove_some == Some(100i32));
+        proof_assert!(remove_some == Some(200i32));
         proof_assert!(remove_none2 == None);
         proof_assert!(map.get(2i32) == None);
 
-        let contains1 = map.contains_ghost(&x1);
-        let contains2 = map.contains_ghost(&x2);
-        let contains3 = map.contains_ghost(&x3);
+        let contains1 = map.contains_ghost(&1);
+        let contains2 = map.contains_ghost(&2);
+        let contains3 = map.contains_ghost(&3);
         proof_assert!(contains1);
         proof_assert!(!contains2);
         proof_assert!(!contains3);
 
-        let get1 = map.get_ghost(&x1);
-        let get2 = map.get_ghost(&x2);
-        let get3 = map.get_ghost(&x3);
+        let get1 = map.get_ghost(&1);
+        let get2 = map.get_ghost(&2);
+        let get3 = map.get_ghost(&3);
         proof_assert!(get1 == Some(&42i32));
         proof_assert!(get2 == None);
         proof_assert!(get3 == None);
