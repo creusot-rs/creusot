6aed59948c5a5f351a0187e1b3dddae9ba2f6346
arnaudgolfouse <arnaud.golfouse@laposte.net>
2025-03-10 16:51:33 +0100

    Specify functions in `std::hint`

---
+++ creusot-contracts/src/std/hint.rs
@@ -0,0 +1,35 @@
+use crate::*;
+
+extern_spec! {
+    mod std {
+        mod hint {
+            #[pure]
+            #[requires(cond)]
+            unsafe fn assert_unchecked(cond: bool) {}
+
+            #[pure]
+            #[ensures(true)]
+            fn black_box<T>(dummy: T) -> T {
+                dummy
+            }
+
+            #[pure]
+            #[requires(true)]
+            #[ensures(true)]
+            fn spin_loop() {}
+
+            #[pure]
+            #[requires(false)]
+            unsafe fn unreachable_unchecked() -> ! {
+                unreachable!()
+            }
+
+            #[cfg(feature = "nightly")]
+            #[pure]
+            #[ensures(result == value)]
+            fn must_use<T>(value: T) -> T {
+                value
+            }
+        }
+    }
+}
