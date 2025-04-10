1bedd15cc6c845529aca6dd28e6a2ccf772578fb
Li-yao Xia <lysxia@gmail.com>
2025-04-10 10:10:41 +0200

    test: Resolve TODOs in closures/09_fnonce_resolve.rs

--- tests/should_succeed/closures/09_fnonce_resolve.rs
+++ tests/should_succeed/closures/09_fnonce_resolve.rs
@@ -10,17 +10,15 @@ pub fn f(c: bool) {
     #[ensures((^*old(bx))@ + (^**old(by))@ == 3)]
     move || {
         if c {
-            // TODO : fix #1072 and remove comments
-            //proof_assert!((^**by)@ == 1);
+            proof_assert!((^**by)@ == 1);
             let bx2 = bx;
             **bx2 += 1;
-            //proof_assert!((^*bx2)@ == 2);
+            proof_assert!((^*bx2)@ == 2);
         } else {
-            // TODO : fix #1072 and remove comments
-            //proof_assert!((^*bx)@ == 1);
+            proof_assert!((^*bx)@ == 1);
             let by2 = by;
             ***by2 += 1;
-            //proof_assert!((^**by2)@ == 2);
+            proof_assert!((^**by2)@ == 2);
         }
     };
     f();
