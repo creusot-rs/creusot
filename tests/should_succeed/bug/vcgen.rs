f6cef421448e2d5a54510bcbe05cc66ea80ba08c
Li-yao Xia <lysxia@gmail.com>
2025-04-25 13:59:01 +0200

    Silence spurious warnings in tests/should_succeed

--- tests/should_succeed/bug/vcgen.rs
+++ tests/should_succeed/bug/vcgen.rs
@@ -7,7 +7,7 @@ use creusot_contracts::{logic::FSet, *};
 #[variant(s.len())]
 #[ensures(s.contains(result))]
 #[ensures(forall<o : _> s.contains(o) ==> o <= result )]
-pub(crate) fn set_max(s: FSet<Int>) -> Int {
+pub fn set_max(s: FSet<Int>) -> Int {
     let x = s.peek();
     let s = s.remove(x);
 
