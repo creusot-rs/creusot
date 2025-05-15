667ed3ab4fa126de5e6fdfc98486b7b7684f0bd8
Jacques-Henri Jourdan <jacques-henri.jourdan@cnrs.fr>
2025-05-15 09:09:13 +0200

    Fix test 603.

--- tests/should_fail/bug/603.rs
+++ tests/should_fail/bug/603.rs
@@ -1,14 +1,10 @@
 extern crate creusot_contracts;
 
-pub struct VecMap<K, V>
-where
-    K: Eq + Ord,
-{
-    pub v: Vec<(K, V)>,
-}
+pub struct S();
 
-impl<K: Ord + Eq, V> ::std::default::Default for VecMap<K, V> {
-    fn default() -> Self {
-        Self { v: Vec::new() }
+impl Iterator for S {
+    type Item = ();
+    fn next(&mut self) -> Option<()> {
+        None
     }
 }
