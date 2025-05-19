d1ecfa4949fcd68a3fb79145adf55fe321b01fed
Li-yao Xia <lysxia@gmail.com>
2025-05-20 00:05:34 +0200

    test

--- tests/should_succeed/lang/const.rs
+++ tests/should_succeed/lang/const.rs
@@ -5,11 +5,23 @@ use creusot_contracts::ensures;
 const FOO: usize = 42;
 
 #[ensures(result == 42usize)]
-pub fn foo() -> usize {
+pub const fn foo() -> usize {
     FOO
 }
 
-pub fn array() -> [usize; 2] {
-    const A: [usize; 2] = [0, 1];
-    A
+#[ensures(result == 9usize)]
+pub const fn array() -> usize {
+    const X: [i32; 9] = [1; 9];
+    X.len()
+}
+
+#[ensures(result == N)]
+pub const fn array_param<const N: usize>() -> usize {
+    let x: [u32; N] = const { [1; N] };
+    x.len()
+}
+
+#[ensures(result@ == N@ + 1)]
+pub const fn add_one<const N: usize>() -> usize {
+    const { N + 1 }
 }
