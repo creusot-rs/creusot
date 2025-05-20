0c68d002932fcd2d58747badc678797093100f52
arnaudgolfouse <arnaud.golfouse@laposte.net>
2025-05-20 16:26:24 +0200

    Documentation for RA

--- creusot-contracts/src/logic/ra/view.rs
+++ creusot-contracts/src/logic/ra/view.rs
@@ -6,6 +6,7 @@ use crate::{
     *,
 };
 
+/// The relation used in [`View`].
 pub trait ViewRel {
     type Auth;
     type Frac: RA;
@@ -20,14 +21,24 @@ pub trait ViewRel {
     fn rel_mono(a: Self::Auth, f1: Self::Frac, f2: Self::Frac);
 }
 
+/// The 'view' Resource Algebra.
+///
+/// This resource is parametrized by a [relation](ViewRel) `R` between an
+/// **authoritative** part (of type `R::Auth`) and a **fractional** part
+/// (of type `R::Frac`).
+///
+/// The authoritative part is unique, while the fractional part might not be. When the
+/// two are present, the relation between the two must hold in order for the view to
+/// be [valid](RA::valid).
 // NOTE: we could add (discardable) fractions for the auth part
-#[allow(dead_code)]
 pub struct View<R>
 where
     R: ViewRel,
 {
+    /// Authoritative part of the view
     // TODO: should the fields be priv?
     pub auth: Option<Excl<R::Auth>>,
+    /// Fractional part of the view
     pub frac: Option<R::Frac>,
 }
 
@@ -35,12 +46,14 @@ impl<R> View<R>
 where
     R: ViewRel,
 {
+    /// Create a new `View` containing an authoritative version of `x`.
     #[logic]
     #[open]
     pub fn mkauth(a: R::Auth) -> Self {
         Self { auth: Some(Excl(a)), frac: None }
     }
 
+    /// Create a new `View` containing a fractional version of `x`.
     #[logic]
     #[open]
     pub fn mkfrac(f: R::Frac) -> Self {
