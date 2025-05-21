f4c8cf4b8896898b93ea76cfbecc4b52c1d5f2a1
arnaudgolfouse <arnaud.golfouse@laposte.net>
2025-05-21 11:26:49 +0200

    Allow reading the Id in ghost code

--- creusot-contracts/src/logic/id.rs
+++ creusot-contracts/src/logic/id.rs
@@ -1,7 +1,7 @@
 use crate::*;
 use ::std::marker::PhantomData;
 
-/// A unique id, usable in logic
+/// A unique id, usable in logic and ghost code
 ///
 /// The only properties of this type are:
 /// - there is an infinite number of ids
@@ -22,3 +22,29 @@ impl Clone for Id {
     }
 }
 impl Copy for Id {}
+
+impl PartialEq for Id {
+    #[trusted]
+    #[pure]
+    #[ensures(result == (*self == *other))]
+    #[allow(unused_variables)]
+    fn eq(&self, other: &Self) -> bool {
+        unreachable!("BUG: called ghost function in normal code")
+    }
+
+    #[pure]
+    #[ensures(result != (*self == *other))]
+    fn ne(&self, other: &Self) -> bool {
+        !self.eq(other)
+    }
+}
+impl Eq for Id {}
+
+impl DeepModel for Id {
+    type DeepModelTy = Id;
+    #[logic]
+    #[open]
+    fn deep_model(self) -> Self::DeepModelTy {
+        self
+    }
+}
