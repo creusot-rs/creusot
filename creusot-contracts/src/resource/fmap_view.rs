7b5bc1e53730b165b5f27166c7057401f5440c80
arnaudgolfouse <arnaud.golfouse@laposte.net>
2025-05-22 21:22:22 +0200

    Rename 'fraction' to 'fragment'

--- creusot-contracts/src/resource/fmap_view.rs
+++ creusot-contracts/src/resource/fmap_view.rs
@@ -17,11 +17,11 @@ struct MapRelation<K, V>(PhantomData<(K, V)>);
 
 impl<K, V> ViewRel for MapRelation<K, V> {
     type Auth = FMap<K, V>;
-    type Frac = FMap<K, Ag<V>>;
+    type Frag = FMap<K, Ag<V>>;
 
     #[logic]
     #[open]
-    fn rel(a: Self::Auth, f: Self::Frac) -> bool {
+    fn rel(a: Self::Auth, f: Self::Frag) -> bool {
         f.valid()
             && pearlite! {
                 forall<k: K> match f.get(k) {
@@ -36,7 +36,7 @@ impl<K, V> ViewRel for MapRelation<K, V> {
     #[requires(f2.incl(f1))]
     #[ensures(Self::rel(a, f2))]
     #[open]
-    fn rel_mono(a: Self::Auth, f1: Self::Frac, f2: Self::Frac) {}
+    fn rel_mono(a: Self::Auth, f1: Self::Frag, f2: Self::Frag) {}
 }
 
 /// Wrapper around a [`Resource`], that allows to agree on the values of a [`FMap`].
@@ -68,11 +68,11 @@ impl<K, V> FMapView<K, V> {
     ///
     /// This is _not_ incompatible with [`Self::is_auth`].
     ///
-    /// Use [`Self::frac`] to get the actual fragment version.
+    /// Use [`Self::frag`] to get the actual fragment version.
     #[predicate]
     #[open(self)]
-    pub fn is_frac(self) -> bool {
-        self.0.val().frac != None
+    pub fn is_frag(self) -> bool {
+        self.0.val().frag != None
     }
 
     /// Get the authoritative version of the map, or the empty map.
@@ -88,18 +88,18 @@ impl<K, V> FMapView<K, V> {
 
     #[logic]
     #[open(self)]
-    fn frac_agree(self) -> FMap<K, Ag<V>> {
-        match self.0.val().frac {
+    fn frag_agree(self) -> FMap<K, Ag<V>> {
+        match self.0.val().frag {
             None => FMap::empty(),
-            Some(frac) => frac,
+            Some(frag) => frag,
         }
     }
 
-    /// Get the fraction version of the map, or the empty map.
+    /// Get the fragment version of the map, or the empty map.
     #[logic]
     #[open(self)]
-    pub fn frac(self) -> FMap<K, V> {
-        self.frac_agree().filter_map(|(_, v)| match v {
+    pub fn frag(self) -> FMap<K, V> {
+        self.frag_agree().filter_map(|(_, v)| match v {
             Ag::Ag(v) => Some(v),
             Ag::Bot => None,
         })
@@ -109,7 +109,7 @@ impl<K, V> FMapView<K, V> {
 impl<K, V> FMapView<K, V> {
     /// Create a new, empty authoritative map.
     #[ensures(result.is_auth())]
-    #[ensures(!result.is_frac())]
+    #[ensures(!result.is_frag())]
     #[ensures(result.auth() == FMap::empty())]
     pub fn new() -> Ghost<Self> {
         let resource = Resource::alloc(snapshot!(View::mkauth(FMap::empty())));
@@ -117,50 +117,50 @@ impl<K, V> FMapView<K, V> {
     }
 
     /// If we have the authoritative version, insert a new element and return the
-    /// corresponding fraction.
+    /// corresponding fragment.
     #[requires(self.is_auth() && !self.auth().contains(*k))]
     #[ensures((^self).is_auth() && (^self).auth() == self.auth().insert(*k, *v))]
-    #[ensures((^self).is_frac() == self.is_frac() && (^self).frac() == self.frac())]
+    #[ensures((^self).is_frag() == self.is_frag() && (^self).frag() == self.frag())]
     #[ensures((^self).id() == self.id())]
-    #[ensures(!result.is_auth() && result.is_frac())]
-    #[ensures(result.frac() == FMap::empty().insert(*k, *v))]
+    #[ensures(!result.is_auth() && result.is_frag())]
+    #[ensures(result.frag() == FMap::empty().insert(*k, *v))]
     #[ensures(result.id() == self.id())]
     #[pure]
     #[allow(unused_variables)]
     pub fn insert(&mut self, k: Snapshot<K>, v: Snapshot<V>) -> Self {
         let new_auth = snapshot!(View::mkauth(self.auth().insert(*k, *v)));
-        let new_total = snapshot!(match self.0@.frac {
+        let new_total = snapshot!(match self.0@.frag {
             None => *new_auth,
-            Some(frac) => new_auth.op(View::mkfrac(frac)),
+            Some(frag) => new_auth.op(View::mkfrag(frag)),
         });
         self.0.update(new_total);
         let right = snapshot!(self.0@);
-        let left = snapshot!(View::mkfrac(FMap::empty().insert(*k, Ag::Ag(*v))));
+        let left = snapshot!(View::mkfrag(FMap::empty().insert(*k, Ag::Ag(*v))));
         Self(self.0.split_off(right, left))
     }
 
-    /// Asserts that the fractional part of `frac` is contained in the authoritative
+    /// Asserts that the fragment part of `frag` is contained in the authoritative
     /// part of `self` (if both exist).
-    #[requires(self.id() == frac.id())]
-    #[requires(self.is_auth() && frac.is_frac())]
-    #[ensures(forall<k: _> match frac.frac().get(k) {
+    #[requires(self.id() == frag.id())]
+    #[requires(self.is_auth() && frag.is_frag())]
+    #[ensures(forall<k: _> match frag.frag().get(k) {
             Some(v) => self.auth().get(k) == Some(v),
             _ => true,
         })]
     #[pure]
     #[allow(unused_variables)]
-    pub fn contains(&self, frac: &Self) {
-        let new_resource = self.0.join_shared(&frac.0);
-        let new_frac = snapshot!(match new_resource@.frac {
+    pub fn contains(&self, frag: &Self) {
+        let new_resource = self.0.join_shared(&frag.0);
+        let new_frag = snapshot!(match new_resource@.frag {
             None => FMap::empty(),
             Some(map) => map,
         });
-        let old_frac = snapshot!(match frac.0@.frac {
+        let old_frag = snapshot!(match frag.0@.frag {
             None => FMap::empty(),
             Some(map) => map,
         });
-        proof_assert!(forall<k: _> match frac.frac().get(k) {
-            Some(v) => new_frac.get(k) == Some(Ag::Ag(v)),
+        proof_assert!(forall<k: _> match frag.frag().get(k) {
+            Some(v) => new_frag.get(k) == Some(Ag::Ag(v)),
             None => true,
         });
     }
