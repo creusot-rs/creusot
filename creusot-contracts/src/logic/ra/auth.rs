3b03e6cd17b5cff20c0e6687084931b919566de4
arnaudgolfouse <arnaud.golfouse@laposte.net>
2025-05-22 21:22:22 +0200

    Rename 'fraction' to 'fragment'

--- creusot-contracts/src/logic/ra/auth.rs
+++ creusot-contracts/src/logic/ra/auth.rs
@@ -13,11 +13,11 @@ where
     T: RA,
 {
     type Auth = T;
-    type Frac = T;
+    type Frag = T;
 
     #[logic]
     #[open]
-    fn rel(a: Self::Auth, f: Self::Frac) -> bool {
+    fn rel(a: Self::Auth, f: Self::Frag) -> bool {
         f.incl(a) && a.valid()
     }
 
@@ -26,14 +26,14 @@ where
     #[requires(Self::rel(a, f1))]
     #[requires(f2.incl(f1))]
     #[ensures(Self::rel(a, f2))]
-    fn rel_mono(a: Self::Auth, f1: Self::Frac, f2: Self::Frac) {}
+    fn rel_mono(a: Self::Auth, f1: Self::Frag, f2: Self::Frag) {}
 }
 
 /// The 'authority' Resource Algebra.
 ///
 /// This is a specialisation of [`View`], where:
-/// - both `Auth` and `Frac` are the same type
-/// - the relation between the two is that `Frac` must always be included in `Auth`
+/// - both `Auth` and `Frag` are the same type
+/// - the relation between the two is that `Frag` must always be included in `Auth`
 pub struct Auth<T: RA>(pub View<AuthViewRel<T>>);
 
 impl<T> Auth<T>
@@ -47,11 +47,11 @@ where
         Auth(View::mkauth(x))
     }
 
-    /// Create a new `Auth` containing a fractional version of `x`.
+    /// Create a new `Auth` containing a fragment version of `x`.
     #[logic]
     #[open]
-    pub fn mkfrac(x: T) -> Self {
-        Auth(View::mkfrac(x))
+    pub fn mkfrag(x: T) -> Self {
+        Auth(View::mkfrag(x))
     }
 }
 
