58b5f12305098a25ecaf0efdfc5febeb4fba4713
arnaudgolfouse <arnaud.golfouse@laposte.net>
2025-05-22 21:22:22 +0200

    Rename 'fraction' to 'fragment'

--- creusot-contracts/src/logic/ra/view.rs
+++ creusot-contracts/src/logic/ra/view.rs
@@ -5,38 +5,43 @@ use crate::{
 
 /// The relation used in [`View`].
 pub trait ViewRel {
+    /// The type of the _authority_ portion of a view
     type Auth;
-    type Frac: RA;
+    /// The type of a _fragment_ portion of a view
+    type Frag: RA;
 
+    /// The relation between the authority and a fragment
     #[logic]
-    fn rel(a: Self::Auth, f: Self::Frac) -> bool;
+    fn rel(a: Self::Auth, f: Self::Frag) -> bool;
 
     #[law]
     #[requires(Self::rel(a, f1))]
     #[requires(f2.incl(f1))]
     #[ensures(Self::rel(a, f2))]
-    fn rel_mono(a: Self::Auth, f1: Self::Frac, f2: Self::Frac);
+    fn rel_mono(a: Self::Auth, f1: Self::Frag, f2: Self::Frag);
 }
 
 /// The 'view' Resource Algebra.
 ///
 /// This resource is parametrized by a [relation](ViewRel) `R` between an
-/// **authoritative** part (of type `R::Auth`) and a **fractional** part
-/// (of type `R::Frac`).
+/// **authoritative** part (of type `R::Auth`) and a **fragment** part
+/// (of type `R::Frag`).
 ///
-/// The authoritative part is unique, while the fractional part might not be. When the
+/// The authoritative part is unique, while the fragment part might not be. When the
 /// two are present, the relation between the two must hold in order for the view to
 /// be [valid](RA::valid).
-// NOTE: we could add (discardable) fractions for the auth part
+// NOTE: we could add (discardable) fragments for the auth part
 pub struct View<R>
 where
     R: ViewRel,
 {
     /// Authoritative part of the view
-    // TODO: should the fields be priv?
+    ///
+    /// Note the [`Excl`], which guarantees that only one [`Resource`] wrapping a
+    /// [`View`] may have the authority part.
     pub auth: Option<Excl<R::Auth>>,
-    /// Fractional part of the view
-    pub frac: Option<R::Frac>,
+    /// Fragment part of the view
+    pub frag: Option<R::Frag>,
 }
 
 impl<R> View<R>
@@ -47,14 +52,14 @@ where
     #[logic]
     #[open]
     pub fn mkauth(a: R::Auth) -> Self {
-        Self { auth: Some(Excl::Excl(a)), frac: None }
+        Self { auth: Some(Excl::Excl(a)), frag: None }
     }
 
-    /// Create a new `View` containing a fractional version of `x`.
+    /// Create a new `View` containing a fragment version of `x`.
     #[logic]
     #[open]
-    pub fn mkfrac(f: R::Frac) -> Self {
-        Self { auth: None, frac: Some(f) }
+    pub fn mkfrag(f: R::Frag) -> Self {
+        Self { auth: None, frag: Some(f) }
     }
 }
 
@@ -65,8 +70,8 @@ where
     #[logic]
     #[open]
     fn op(self, other: Self) -> Self {
-        let (auth, frac) = (self.auth, self.frac).op((other.auth, other.frac));
-        Self { auth, frac }
+        let (auth, frag) = (self.auth, self.frag).op((other.auth, other.frag));
+        Self { auth, frag }
     }
 
     #[logic]
@@ -74,12 +79,12 @@ where
     fn valid(self) -> bool {
         pearlite! {
             match self {
-                Self { auth: Some(Excl::Excl(a)), frac: Some(f) } => f.valid() && R::rel(a, f),
-                // TODO: why is this condition necessary?
-                Self { auth: None, frac: Some(f) } => f.valid() && exists<a: R::Auth> R::rel(a, f),
-                Self { auth: Some(Excl::Excl(_)), frac: None } => true,
-                Self { auth: None, frac: None } => true,
-                Self { auth: Some(Excl::Bot), frac: _ } => false,
+                Self { auth: Some(Excl::Excl(a)), frag: Some(f) } => f.valid() && R::rel(a, f),
+                // TODO: why is this condition necessary? Try to remove it
+                Self { auth: None, frag: Some(f) } => f.valid() && exists<a: R::Auth> R::rel(a, f),
+                Self { auth: Some(Excl::Excl(_)), frag: None } => true,
+                Self { auth: None, frag: None } => true,
+                Self { auth: Some(Excl::Bot), frag: _ } => false,
             }
         }
     }
@@ -88,14 +93,14 @@ where
     #[open]
     #[ensures(result == (exists<c: Self> self.op(c) == other))]
     fn incl(self, other: Self) -> bool {
-        (self.auth, self.frac).incl((other.auth, other.frac))
+        (self.auth, self.frag).incl((other.auth, other.frag))
     }
 
     #[logic]
     #[open]
     #[ensures(result == (self.op(self) == self))]
     fn idemp(self) -> bool {
-        (self.auth, self.frac).idemp()
+        (self.auth, self.frag).idemp()
     }
 
     #[law]
@@ -112,7 +117,7 @@ where
     #[open(self)]
     #[ensures(self.op(b).valid() ==> self.valid())]
     fn valid_op(self, b: Self) {
-        let _ = <R::Frac as RA>::valid_op;
+        let _ = <R::Frag as RA>::valid_op;
     }
 
     #[logic]
@@ -125,6 +130,6 @@ where
     )]
     fn maximal_idemp(self) {
         self.auth.maximal_idemp();
-        self.frac.maximal_idemp();
+        self.frag.maximal_idemp();
     }
 }
