45c84cec2977e92af3fc41b2674e5c57a18b8a02
arnaudgolfouse <arnaud.golfouse@laposte.net>
2025-05-23 20:21:29 +0200

    Optimize useless gotos

--- creusot/src/backend/optimization.rs
+++ creusot/src/backend/optimization.rs
@@ -1,5 +1,6 @@
 mod constant_propagation;
 mod invariants;
+mod useless_goto;
 
 use crate::{
     ctx::{BodyId, TranslationCtx},
@@ -9,6 +10,7 @@ use crate::{
 /// Various optimizations performed on fMIR:
 /// - constant propagation
 /// - inference of prophetic invariants
+/// - remove useless gotos
 pub(crate) fn optimizations<'tcx>(
     ctx: &TranslationCtx<'tcx>,
     body: &mut Body<'tcx>,
@@ -16,4 +18,5 @@ pub(crate) fn optimizations<'tcx>(
 ) {
     constant_propagation::propagate_constants(body);
     invariants::infer_proph_invariants(ctx, body, body_id);
+    useless_goto::remove_useless_gotos(body);
 }
