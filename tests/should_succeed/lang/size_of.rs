9fb7284d4191684c3e9054439c531ca20e055a61
Li-yao Xia <lysxia@gmail.com>
2025-06-13 20:17:25 +0200

    Update tests

---
+++ tests/should_succeed/lang/size_of.rs
@@ -0,0 +1,24 @@
+extern crate creusot_contracts;
+use creusot_contracts::*;
+
+pub fn f() {
+    proof_assert!(size_of_logic::<bool>() == 1);
+    proof_assert!(size_of_logic::<char>() == 4);
+    proof_assert!(size_of_logic::<u8>() == 1);
+    proof_assert!(size_of_logic::<u64>() == 8);
+    proof_assert!(size_of_logic::<()>() == 0);
+    proof_assert!(size_of_logic::<[u32; 5]>() == 20);
+    proof_assert!(size_of_logic::<[(); 5]>() == 0);
+}
+
+pub fn g<T>() {
+    let t2 = size_of::<[T; 2]>();
+    proof_assert!(t2@ == 2 * size_of_logic::<T>());
+    proof_assert!(size_of_logic::<&T>() == size_of_logic::<usize>());
+    proof_assert!(size_of_logic::<*const T>() == size_of_logic::<usize>());
+    proof_assert!(size_of_logic::<Box<T>>() == size_of_logic::<usize>());
+    proof_assert!(size_of_logic::<Option<&T>>() == size_of_logic::<usize>());
+    proof_assert!(size_of_logic::<Option<Box<T>>>() == size_of_logic::<usize>());
+    proof_assert!(size_of_logic::<[T; 5]>() == 5 * size_of_logic::<T>());
+    proof_assert!(size_of_logic::<[T; 0]>() == 0);
+}
