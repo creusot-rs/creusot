7c0498419c903573fd1e9a2f8c6a05438838750f
Li-yao Xia <lysxia@gmail.com>
2025-06-19 18:52:09 +0200

    Update test

--- tests/should_succeed/lang/cast_ptr.rs
+++ tests/should_succeed/lang/cast_ptr.rs
@@ -1,6 +1,12 @@
 extern crate creusot_contracts;
 use creusot_contracts::*;
 
-pub fn f<T: ?Sized>(p: *const T) {
-    proof_assert!(p == (p as *mut T) as *const T)
+#[ensures(result == p as *mut T)]
+pub fn f<T: ?Sized>(p: *const T) -> *mut T {
+    p as *mut T
+}
+
+#[ensures(result == p as *const T)]
+pub fn g<T: ?Sized>(p: *mut T) -> *const T {
+    p as *const T
 }
