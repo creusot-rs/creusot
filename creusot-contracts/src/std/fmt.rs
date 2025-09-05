3b9cac7c3a358928a6f42adb1f647739ec7e4215
Li-yao Xia <lysxia@gmail.com>
2025-09-05 18:35:00 +0200

    Fix error when translating `dyn`
    Fix a check to allow `dyn` to be translated to an opaque type
    
    This allows translating `derive(Debug)`: `Debug::fmt` uses the type
    `std::fmt::Formatter` which contains a field of type `&dyn Write`.

--- creusot-contracts/src/std/fmt.rs
+++ creusot-contracts/src/std/fmt.rs
@@ -1,17 +1,132 @@
 use crate::*;
 #[cfg(creusot)]
-use ::std::fmt::{Arguments, Debug, Formatter};
+use ::std::fmt::{Arguments, Debug, Error, Formatter};
 
 extern_spec! {
     mod core {
         mod fmt {
             impl<'a> Formatter<'a> {
                 #[requires(true)]
+                fn write_str(&mut self, data: &str) -> Result<(), Error>;
+
+                #[requires(true)]
                 fn debug_struct_field1_finish<'b>(
                     &'b mut self,
                     name: &str,
                     name1: &str,
                     value1: &dyn Debug,
+                ) -> ::std::fmt::Result;
+
+                #[requires(true)]
+                fn debug_struct_field2_finish<'b>(
+                    &'b mut self,
+                    name: &str,
+                    name1: &str,
+                    value1: &dyn Debug,
+                    name2: &str,
+                    value2: &dyn Debug,
+                ) -> ::std::fmt::Result;
+
+                #[requires(true)]
+                fn debug_struct_field3_finish<'b>(
+                    &'b mut self,
+                    name: &str,
+                    name1: &str,
+                    value1: &dyn Debug,
+                    name2: &str,
+                    value2: &dyn Debug,
+                    name3: &str,
+                    value3: &dyn Debug,
+                ) -> ::std::fmt::Result;
+
+                #[requires(true)]
+                fn debug_struct_field4_finish<'b>(
+                    &'b mut self,
+                    name: &str,
+                    name1: &str,
+                    value1: &dyn Debug,
+                    name2: &str,
+                    value2: &dyn Debug,
+                    name3: &str,
+                    value3: &dyn Debug,
+                    name4: &str,
+                    value4: &dyn Debug,
+                ) -> ::std::fmt::Result;
+
+                #[requires(true)]
+                fn debug_struct_field5_finish<'b>(
+                    &'b mut self,
+                    name: &str,
+                    name1: &str,
+                    value1: &dyn Debug,
+                    name2: &str,
+                    value2: &dyn Debug,
+                    name3: &str,
+                    value3: &dyn Debug,
+                    name4: &str,
+                    value4: &dyn Debug,
+                    name5: &str,
+                    value5: &dyn Debug,
+                ) -> ::std::fmt::Result;
+
+                #[requires(true)]
+                fn debug_struct_fields_finish<'b>(
+                    &'b mut self,
+                    name: &str,
+                    names: &[&str],
+                    values: &[&dyn Debug],
+                ) -> ::std::fmt::Result;
+
+                #[requires(true)]
+                fn debug_tuple_field1_finish<'b>(
+                    &'b mut self,
+                    name: &str,
+                    value1: &dyn Debug,
+                ) -> ::std::fmt::Result;
+
+                #[requires(true)]
+                fn debug_tuple_field2_finish<'b>(
+                    &'b mut self,
+                    name: &str,
+                    value1: &dyn Debug,
+                    value2: &dyn Debug,
+                ) -> ::std::fmt::Result;
+
+                #[requires(true)]
+                fn debug_tuple_field3_finish<'b>(
+                    &'b mut self,
+                    name: &str,
+                    value1: &dyn Debug,
+                    value2: &dyn Debug,
+                    value3: &dyn Debug,
+                ) -> ::std::fmt::Result;
+
+                #[requires(true)]
+                fn debug_tuple_field4_finish<'b>(
+                    &'b mut self,
+                    name: &str,
+                    value1: &dyn Debug,
+                    value2: &dyn Debug,
+                    value3: &dyn Debug,
+                    value4: &dyn Debug,
+                ) -> ::std::fmt::Result;
+
+                #[requires(true)]
+                fn debug_tuple_field5_finish<'b>(
+                    &'b mut self,
+                    name: &str,
+                    value1: &dyn Debug,
+                    value2: &dyn Debug,
+                    value3: &dyn Debug,
+                    value4: &dyn Debug,
+                    value5: &dyn Debug,
+                ) -> ::std::fmt::Result;
+
+                #[requires(true)]
+                fn debug_tuple_fields_finish<'b>(
+                    &'b mut self,
+                    name: &str,
+                    values: &[&dyn Debug],
                 ) -> ::std::fmt::Result;
             }
         }
