8c47a52cd26a5ef288d9c2a82cca614f10b31b49
Li-yao Xia <lysxia@gmail.com>
2025-09-09 09:43:47 +0200

    refines: parse function calls

--- tests/should_succeed/specification/refines.rs
+++ tests/should_succeed/specification/refines.rs
@@ -1,12 +1,22 @@
 extern crate creusot_contracts;
 use creusot_contracts::*;
 
-#[trusted]
 pub fn foo(x: i32) -> i32 {
     x
+}
+
+pub fn bar(x: i32) -> i32 {
+    let y = foo(x);
+    y
 }
 
 #[refines(foo)]
 pub fn foo2(x: i32) -> i32 {
     x
+}
+
+#[refines(bar)]
+pub fn bar2(x: i32) -> i32 {
+    let y = foo2(x);
+    y
 }
