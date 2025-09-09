4c70400ac020074951a96d8bc222a1bed18fb26f
Arnaud Golfouse <arnaud.golfouse@laposte.net>
2025-09-09 15:15:05 +0200

    Fix union_find proof

--- tests/should_succeed/union_find.rs
+++ tests/should_succeed/union_find.rs
@@ -1,4 +1,4 @@
-// TACTIC +compute_in_goal
+// TACTIC +compute_specified,+inline_goal
 extern crate creusot_contracts;
 
 // This proof is largely adapted from the one in Vocal (see https://github.com/ocaml-gospel/vocal/blob/main/proofs/why3/UnionFind_impl.mlw)
@@ -70,7 +70,7 @@ mod implementation {
             pearlite! {
             // this invariant was not in the why3 proof: it is here because of the specifics of `DeepModel` and equality in Creusot
             (forall<e1, e2> self.0.domain.contains(e1) && self.0.domain.contains(e2) && e1.deep_model() == e2.deep_model() ==> e1 == e2) &&
-            (forall<e> self.0.domain.contains(e) ==>
+            (forall<e> #[trigger(self.0.domain.contains(e))] self.0.domain.contains(e) ==>
                 // this invariant was not in the why3 proof: it ensures that the keys and the payloads of `perm` agree
                 self.0.perms.contains(e) &&
                 self.0.perms[e].ptr() == e.0 &&
