ea5ef58bc5dc9cc5523bbe727dcc51d2acf4f32e
Arnaud Golfouse <arnaud.golfouse@laposte.net>
2025-09-11 14:28:27 +0200

    Failing tests for incorrect permissions

---
+++ tests/should_fail/wrong_permissions.rs
@@ -0,0 +1,31 @@
+// WHY3PROVE
+extern crate creusot_contracts;
+use creusot_contracts::{
+    pcell::{PCell, PCellOwn},
+    ptr_own::PtrOwn,
+    *,
+};
+
+pub fn unknown_pcell_permission(cell: &PCell<i32>, perm: Ghost<&PCellOwn<i32>>) {
+    // does not work: we don't know if cell and perm have the same id
+    let _ = unsafe { cell.borrow(perm) };
+}
+pub fn wrong_pcell_permission() {
+    let (cell, _) = PCell::new(1i32);
+    let (_, perm) = PCell::new(1i32);
+
+    // does not work: we know that `perm` is not `cell`'s permission
+    let _ = unsafe { cell.borrow(perm.borrow()) };
+}
+
+pub fn unknown_ptr_own_permission(ptr: *const i32, perm: Ghost<&PtrOwn<i32>>) {
+    // does not work: we don't know if ptr and perm have the same id
+    let _ = unsafe { PtrOwn::as_ref(ptr, perm) };
+}
+pub fn wrong_ptr_own_permission() {
+    let (ptr, _) = PtrOwn::new(1i32);
+    let (_, perm) = PtrOwn::new(1i32);
+
+    // does not work: we know that `perm` is not `ptr`'s permission
+    let _ = unsafe { PtrOwn::as_ref(ptr, perm.borrow()) };
+}
