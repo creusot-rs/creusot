3ebcf0f85eb33d262289b88e705c63586c42946a
Li-yao Xia <lysxia@gmail.com>
2025-10-06 11:18:02 +0200

    Add extern_spec for intrinsics::{unreachable, assume}

--- creusot-contracts/src/std/intrinsics.rs
+++ creusot-contracts/src/std/intrinsics.rs
@@ -3,7 +3,21 @@ use crate::*;
 extern_spec! {
     mod core {
         mod intrinsics {
+            #[check(ghost)]
             fn ub_checks() -> bool;
+
+            #[check(ghost)]
+            #[requires(false)]
+            unsafe fn unreachable() -> !;
+
+            #[erasure]
+            #[check(ghost)]
+            #[requires(b)]
+            unsafe fn assume(b: bool) {
+                if !b {
+                    unsafe { core::intrinsics::unreachable() }
+                }
+            }
         }
     }
 }
