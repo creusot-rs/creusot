702753173e65b501b4ea383668a5f6d2e7d95a5b
Arnaud Golfouse <arnaud.golfouse@laposte.net>
2025-10-08 14:57:40 +0200

    Improve incorrect law declaration error

--- creusot/src/validate/incorrect_attributes.rs
+++ creusot/src/validate/incorrect_attributes.rs
@@ -1,4 +1,4 @@
-use crate::contracts_items::{is_check_ghost, is_check_terminates, is_logic};
+use crate::contracts_items::{is_check_ghost, is_check_terminates, is_law, is_logic};
 use rustc_hir::def_id::DefId;
 use rustc_middle::ty::TyCtxt;
 
@@ -14,6 +14,14 @@ const INCOMPATIBLE_ATTRIBUTES: &[(AttributeFn, AttributeFn, &str)] = &[
 ];
 
 pub(super) fn validate_incorrect_attributes(tcx: TyCtxt, def_id: DefId) {
+    if is_law(tcx, def_id)
+        && !(tcx.trait_of_assoc(def_id).is_some() || tcx.trait_impl_of_assoc(def_id).is_some())
+    {
+        tcx.dcx().span_err(
+            tcx.def_span(def_id),
+            "Laws can only be declared in trait definitions or implementations",
+        );
+    }
     for &(attr1, attr2, reason) in INCOMPATIBLE_ATTRIBUTES {
         if attr1(tcx, def_id) && attr2(tcx, def_id) {
             tcx.dcx().span_err(tcx.def_span(def_id), reason);
