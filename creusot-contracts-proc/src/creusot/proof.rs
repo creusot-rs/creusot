4c42b462a07c6bf422e3c750201010a36ed27c57
Li-yao Xia <lysxia@gmail.com>
2025-10-27 15:59:19 +0100

    Disable 'unused' lints in logic

--- creusot-contracts-proc/src/creusot/proof.rs
+++ creusot-contracts-proc/src/creusot/proof.rs
@@ -20,6 +20,7 @@ pub fn proof_assert(assertion: TS1) -> TS1 {
                 #[creusot::no_translate]
                 #[creusot::spec]
                 #[creusot::spec::assert]
+                #[allow(unused)]
                 || -> bool #assert_body;
         }
     })
@@ -34,6 +35,7 @@ pub fn snapshot(snapshot: TS1) -> TS1 {
             #[creusot::no_translate]
             #[creusot::spec]
             #[creusot::spec::snapshot]
+            #[allow(unused)]
             || #snap_body
         )
     })
