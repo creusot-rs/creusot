b599d4fa4d743138d68227a21fa20bd5d29ab99d
Li-yao Xia <lysxia@gmail.com>
2025-11-03 17:14:32 +0100

    Remember discarded stuff

--- creusot/src/cleanup_spec_closures.rs
+++ creusot/src/cleanup_spec_closures.rs
@@ -5,8 +5,7 @@ use rustc_hir::def_id::{DefId, LocalDefId};
 use rustc_index::{Idx, IndexVec};
 use rustc_middle::{
     mir::{
-        self,
-        AggregateKind, BasicBlock, BasicBlockData, Body, Local, Location, Rvalue, SourceInfo,
+        self, AggregateKind, BasicBlock, BasicBlockData, Body, Local, Location, Rvalue, SourceInfo,
         StatementKind, Terminator, TerminatorKind,
         visit::{MutVisitor, PlaceContext},
     },
@@ -18,6 +17,10 @@ thread_local! {
     pub static PEARLITE_MIR:
         RefCell<HashMap<LocalDefId, IndexVec<BasicBlock, BasicBlockData<'static>>>> =
         RefCell::new(HashMap::new());
+
+    pub static LOGICAL_STUFF:
+        RefCell<HashMap<LocalDefId, LogicalStuff<'static>>> =
+        RefCell::new(HashMap::new());
 }
 
 /// Hide non-linear specification code from the borrow checker
@@ -27,20 +30,26 @@ thread_local! {
 /// To prevent the closures from intererring with the borrow checking of the surrounding environment, we replace the MIR body of the closure with an empty loop and remove all of the arguments to the closure in the surrounding MIR.
 pub(crate) fn cleanup_spec_closures<'tcx>(tcx: TyCtxt<'tcx>, def_id: DefId, body: &mut Body<'tcx>) {
     trace!("cleanup_spec_closures: {:?}", def_id);
+    let local = def_id.expect_local();
 
     if is_no_translate(tcx, def_id) || is_logic(tcx, def_id) {
         trace!("replacing function body");
         PEARLITE_MIR.with(|map| {
             let bb = std::mem::replace(body.basic_blocks_mut(), make_loop());
-            // SAFETY: Consumers cast lifetime back to 'tcx
+            // SAFETY: Consumers cast the lifetime back to 'tcx
             let bb = unsafe { std::mem::transmute(bb) };
-            map.borrow_mut().insert(def_id.expect_local(), bb);
+            map.borrow_mut().insert(local, bb);
         });
     } else {
         let mut cleanup = NoTranslateNoMoves::new(tcx);
         cleanup.visit_body(body);
-
-        cleanup_statements(body, &cleanup.unused);
+        let closures = cleanup.closures;
+        let assigns = cleanup_statements(body, &cleanup.unused);
+        LOGICAL_STUFF.with(|map| {
+            // SAFETY: Consumers cast the lifetime back to 'tcx
+            let stuff = unsafe { std::mem::transmute(LogicalStuff { closures, assigns }) };
+            map.borrow_mut().insert(local, stuff);
+        });
         let map = map_locals(&mut body.local_decls, &cleanup.unused);
         let mut updater = LocalUpdater { map, tcx };
         updater.visit_body(body);
@@ -49,19 +58,34 @@ pub(crate) fn cleanup_spec_closures<'tcx>(tcx: TyCtxt<'tcx>, def_id: DefId, body
     }
 }
 
-fn cleanup_statements<'tcx>(body: &mut Body<'tcx>, unused: &IndexSet<Local>) {
+struct LogicalStuff<'tcx> {
+    closures: SpecClosures<'tcx>,
+    assigns: SpecAssigns<'tcx>,
+}
+
+type SpecClosures<'tcx> = HashMap<DefId, IndexVec<FieldIdx, mir::Operand<'tcx>>>;
+type SpecAssigns<'tcx> = HashMap<mir::Place<'tcx>, Rvalue<'tcx>>;
+
+fn cleanup_statements<'tcx>(body: &mut Body<'tcx>, unused: &IndexSet<Local>) -> SpecAssigns<'tcx> {
+    let mut assigns: SpecAssigns = HashMap::new();
     for data in body.basic_blocks_mut() {
         data.statements.retain(|statement| match &statement.kind {
             StatementKind::StorageLive(local) | StatementKind::StorageDead(local) => {
                 !unused.contains(local)
             }
             StatementKind::PlaceMention(place) => !unused.contains(&place.local),
-            StatementKind::Assign(box (place, _)) | StatementKind::FakeRead(box (_, place)) => {
-                !unused.contains(&place.local)
+            StatementKind::Assign(box (place, rvalue)) => {
+                let dropped = unused.contains(&place.local);
+                if dropped {
+                    assigns.insert(*place, rvalue.clone());
+                }
+                !dropped
             }
+            StatementKind::FakeRead(box (_, place)) => !unused.contains(&place.local),
             _ => true,
         })
     }
+    assigns
 }
 
 pub(crate) fn make_loop<'tcx>() -> IndexVec<BasicBlock, BasicBlockData<'tcx>> {
@@ -79,16 +103,12 @@ pub(crate) fn make_loop<'tcx>() -> IndexVec<BasicBlock, BasicBlockData<'tcx>> {
 pub struct NoTranslateNoMoves<'tcx> {
     pub tcx: TyCtxt<'tcx>,
     pub unused: IndexSet<Local>,
-    pub closures: HashMap<DefId, IndexVec<FieldIdx, mir::Operand<'tcx>>>,
+    pub closures: SpecClosures<'tcx>,
 }
 
 impl<'tcx> NoTranslateNoMoves<'tcx> {
     fn new(tcx: TyCtxt<'tcx>) -> Self {
-        Self {
-            tcx,
-            unused: Default::default(),
-            closures: Default::default(),
-        }
+        Self { tcx, unused: IndexSet::new(), closures: HashMap::new() }
     }
 }
 
