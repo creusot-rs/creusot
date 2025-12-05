42e169ef8c9aed5fe40ea62214a31fbd0555eddf
Jacques-Henri Jourdan <jacques-henri.jourdan@cnrs.fr>
2025-12-05 01:58:47 +0100

    Improve useless_goto optimization.

--- creusot/src/backend/optimization/useless_goto.rs
+++ creusot/src/backend/optimization/useless_goto.rs
@@ -1,69 +1,120 @@
-use crate::translation::fmir::{self, Block, Branches, Terminator};
-use indexmap::IndexMap;
-use rustc_middle::mir::BasicBlock;
+use crate::translation::fmir::{self, Block, Terminator};
+use indexmap::{IndexMap, map::Entry};
+use rustc_middle::mir::{BasicBlock, START_BLOCK};
 
-fn get_goto_block(b: &Block) -> Option<BasicBlock> {
-    match b {
-        // do it like this so we don't forget fields
-        Block { invariants, variant, stmts, terminator: Terminator::Goto(bb) }
-            if stmts.is_empty() && invariants.is_empty() && variant.is_none() =>
-        {
-            Some(*bb)
-        }
-        _ => None,
+fn is_simple_block(b: &Block) -> bool {
+    if let Block {
+        invariants,
+        variant,
+        stmts,
+        terminator: Terminator::Goto(_) | Terminator::Abort(_) | Terminator::Return,
+    } = b
+    {
+        stmts.is_empty() && invariants.is_empty() && variant.is_none()
+    } else {
+        false
     }
 }
 
-/// Remove the common pattern:
-/// ```text
-/// bb1:
-///     ...
-///     goto bb2
-/// bb2:
-///     goto bb3
-/// bb3:
-///     ...
-/// ```
-/// `bb2` is useless here.
+/// Remove:
+/// - Unreachable blocks
+/// - Shortcut blocks (i.e., blocks that only contain a goto)
+/// - Jump to a block that only returns or aborts
+/// - blocks that only have a predecessor which is a goto
+///
+/// These optimization should not change at all the VCs, but they make the COMA code much
+/// more human-readable.
 pub(super) fn remove_useless_gotos(body: &mut fmir::Body) {
+    // Compute shortcuts
     let mut shortcuts_to = IndexMap::new();
+    let mut short_term = IndexMap::new();
     for (&bb, block) in body.blocks.iter() {
-        if let Some(to_bb) = get_goto_block(block) {
-            shortcuts_to.insert(bb, to_bb);
+        if is_simple_block(block) {
+            match &block.terminator {
+                Terminator::Goto(to_bb) => {
+                    shortcuts_to.insert(bb, *to_bb);
+                }
+                trm => {
+                    short_term.insert(bb, trm.clone());
+                }
+            }
         }
     }
 
-    let mut replace_bb = |bb2: &mut _| find_shortcut(&mut shortcuts_to, bb2);
     for (_, block) in body.blocks.iter_mut() {
-        match &mut block.terminator {
-            Terminator::Goto(bb) => replace_bb(bb),
-            Terminator::Switch(_, branches) => match branches {
-                Branches::Int(items, def) => {
-                    replace_bb(def);
-                    for (_, to) in items {
-                        replace_bb(to);
-                    }
+        // Remove goto chains
+        for tgt in block.terminator.targets_mut() {
+            find_shortcut(&mut shortcuts_to, tgt)
+        }
+
+        // Remove gotos to return/abort
+        if let Terminator::Goto(nxt) = block.terminator
+            && let Some(term) = short_term.get(&nxt)
+        {
+            block.terminator = term.clone()
+        }
+    }
+
+    // Remove shortcut at the first block
+    let start_block = &body.blocks[&START_BLOCK];
+    if is_simple_block(start_block)
+        && let Terminator::Goto(new_start) = start_block.terminator
+        && new_start != START_BLOCK
+    {
+        let [Some(b1), Some(b2)] = body.blocks.get_disjoint_mut([&START_BLOCK, &new_start]) else {
+            unreachable!()
+        };
+        std::mem::swap(b1, b2);
+
+        for (_, block) in body.blocks.iter_mut() {
+            for tgt in block.terminator.targets_mut() {
+                if *tgt == new_start {
+                    *tgt = START_BLOCK
                 }
-                Branches::Uint(items, def) => {
-                    replace_bb(def);
-                    for (_, to) in items {
-                        replace_bb(to);
-                    }
-                }
-                Branches::Constructor(_, _, cases, def) => {
-                    if let Some(def) = def {
-                        replace_bb(def);
-                    }
-                    for (_, to) in cases {
-                        replace_bb(to);
-                    }
-                }
-                Branches::Bool(to1, to2) => {
-                    replace_bb(to1);
-                    replace_bb(to2);
-                }
-            },
-            Terminator::Return | Terminator::Abort(_) => {}
+            }
+        }
+    }
+
+    // Count predecessors for each block
+    let mut cnt_prev = IndexMap::from([(START_BLOCK, 1u32)]);
+    for (_, block) in body.blocks.iter() {
+        for tgt in block.terminator.targets() {
+            *cnt_prev.entry(tgt).or_insert(0) += 1;
+        }
+    }
+
+    // Remove orphan blocks
+    let mut stk: Vec<BasicBlock> =
+        body.blocks.keys().copied().filter(|bb| !cnt_prev.contains_key(bb)).collect();
+    while let Some(bb) = stk.pop() {
+        for tgt in body.blocks.swap_remove(&bb).unwrap().terminator.targets() {
+            let Entry::Occupied(mut e) = cnt_prev.entry(tgt) else { unreachable!() };
+            *e.get_mut() -= 1;
+            if *e.get() == 0 {
+                stk.push(tgt);
+                e.swap_remove();
+            }
+        }
+    }
+
+    // Merge when goto to a block with only one predecessor
+    for bb in body.blocks.keys().copied().collect::<Vec<_>>() {
+        loop {
+            let Some(&Block { terminator: Terminator::Goto(nextbb), .. }) = body.blocks.get(&bb)
+            else {
+                break;
+            };
+            if cnt_prev[&nextbb] > 1 || bb == nextbb {
+                break;
+            }
+            let Entry::Occupied(e) = body.blocks.entry(nextbb) else { unreachable!() };
+            if !e.get().invariants.is_empty() || e.get().variant.is_some() {
+                break;
+            }
+            let Block { invariants: _, variant: _, stmts, terminator } = e.swap_remove();
+            body.blocks[&bb].stmts.extend(stmts);
+            body.blocks[&bb].terminator = terminator;
+            assert_eq!(cnt_prev.swap_remove(&nextbb), Some(1));
         }
     }
 }
