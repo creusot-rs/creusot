4318b53992ca90e5a45b51ce1f3e6ffeebefc252
Vincent Lafeychine <vincent.lafeychine@proton.me>
2025-12-08 16:42:58 +0100

    feat(std::thread): Add Scope and related functions

--- creusot-contracts/src/std/thread.rs
+++ creusot-contracts/src/std/thread.rs
@@ -1,5 +1,5 @@
 use crate::{ghost::local_invariant::Tokens, prelude::*};
-use ::std::thread::JoinHandle;
+use ::std::thread::{self, JoinHandle, ScopedJoinHandle, ThreadId};
 
 /// Extension trait for [`JoinHandle`].
 pub trait JoinHandleExt<T> {
@@ -9,6 +9,13 @@ pub trait JoinHandleExt<T> {
 }
 
 impl<T> JoinHandleExt<T> for JoinHandle<T> {
+    #[logic(opaque)]
+    fn valid_result(self, _x: T) -> bool {
+        dead
+    }
+}
+
+impl<T> JoinHandleExt<T> for ScopedJoinHandle<'_, T> {
     #[logic(opaque)]
     fn valid_result(self, _x: T) -> bool {
         dead
@@ -28,13 +35,24 @@ extern_spec! {
                 #[ensures(true)] // no spec, but you can call this if you want
                 fn is_finished(&self) -> bool;
             }
+
+            impl<T> ScopedJoinHandle<'_, T> {
+                #[ensures(match result {
+                    Ok(res) => self.valid_result(res),
+                    Err(_) => true,
+                })]
+                fn join(self) -> ::std::thread::Result<T>;
+
+                #[ensures(true)] // no spec, but you can call this if you want
+                fn is_finished(&self) -> bool;
+            }
         }
     }
 }
 
 /// Creusot wrapper around [`std::thread::spawn`].
 ///
-/// The only difference is that the closure gives access to a fresh
+/// The only difference is that the closure gives access to a fresh token object
 #[requires(forall<t: Ghost<Tokens>> (forall<ns> t.contains(ns)) ==> f.precondition((t,)))]
 #[ensures(exists<t: Ghost<Tokens>> (forall<ns> t.contains(ns)) && forall<r> result.valid_result(r) ==> f.postcondition_once((t,), r))]
 #[trusted]
@@ -44,4 +62,33 @@ where
     T: Send + 'static,
 {
     ::std::thread::spawn(|| f(Tokens::new()))
+}
+
+/// Creusot's replacement for [`Scope`].
+pub struct Scope<'scope, 'env: 'scope> {
+    inner: &'scope thread::Scope<'scope, 'env>,
+}
+
+impl<'scope, 'env: 'scope> Scope<'scope, 'env> {
+    #[requires(forall<t: Ghost<Tokens>> (forall<ns> t.contains(ns)) ==> f.precondition((t,)))]
+    #[ensures(exists<t: Ghost<Tokens>> (forall<ns> t.contains(ns)) && forall<r> result.valid_result(r) ==> f.postcondition_once((t,), r))]
+    #[trusted]
+    pub fn spawn<F, T>(&mut self, f: F) -> ScopedJoinHandle<'scope, T>
+    where
+        F: FnOnce(Ghost<Tokens>) -> T + Send + 'scope,
+        T: Send + 'scope,
+    {
+        self.inner.spawn(|| f(Tokens::new()))
+    }
+}
+
+/// Creusot wrapper around [`std::thread::scope`].
+#[requires(forall<s> f.precondition((s,)))]
+#[ensures(exists<s> f.postcondition_once((s,),result))]
+#[trusted]
+pub fn scope<'env, F, T>(f: F) -> T
+where
+    F: for<'scope> FnOnce(&mut Scope<'scope, 'env>) -> T,
+{
+    ::std::thread::scope(|s| f(&mut Scope { inner: s }))
 }
