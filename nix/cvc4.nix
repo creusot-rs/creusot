ebc15c76d778bb33e73ddd36412230f5d66c6a1c
Vincent Lafeychine <vincent.lafeychine@proton.me>
2025-12-31 15:14:12 +0100

    feat(nix): Statically CVC4

--- nix/cvc4.nix
+++ nix/cvc4.nix
@@ -4,7 +4,17 @@
   sha256,
 }:
 with pkgs; let
-  cvc4-cryptominisat = pkgs.cryptominisat.overrideAttrs (old: {
+  cvc4-cadical = pkgsStatic.cadical.overrideAttrs {
+    prePatch = ''
+      sed -i -e '104d' test/api/run.sh
+    '';
+  };
+
+  cvc4-cln = pkgsStatic.cln.overrideAttrs {
+    NIX_CFLAGS_COMPILE = "-DHZ=100";
+  };
+
+  cvc4-cryptominisat = pkgsStatic.cryptominisat.overrideAttrs {
     src = fetchFromGitHub {
       owner = "msoos";
       repo = "cryptominisat";
@@ -14,13 +24,17 @@ with pkgs; let
 
     cmakeFlags = [
       "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
-      "-DENABLE_PYTHON_INTERFACE=OFF"
+      "-DENABLE_PYTHON_INTERFACE=0"
+
+      "-DBUILD_SHARED_LIBS=0"
+      "-DSTATICCOMPILE=1"
     ];
 
     patchPhase = ''
+      sed -i -e '31,36d' src/main_exe.cpp
       sed -i -e '28i#include <cstdint>' src/ccnr.h
     '';
-  });
+  };
 in
   stdenv.mkDerivation {
     inherit (cvc4) meta patches pname preConfigure;
@@ -33,20 +47,25 @@ in
       hash = sha256;
     };
 
-    nativeBuildInputs = [pkg-config cmake];
-
-    buildInputs = [
+    nativeBuildInputs = [
+      cmake
+      pkg-config
       antlr3_4
-      cadical
-      cln
-      cvc4-cryptominisat
-      gmp
       jdk
-      libantlr3c
       python3
       python3.pkgs.toml
+    ];
+
+    buildInputs = with pkgsStatic; [
+      cvc4-cadical
+      cvc4-cln
+      cvc4-cryptominisat
+      libantlr3c
+      pkgs.glibc.static
       symfpu
     ];
+
+    outputs = ["out" "dev" "man"];
 
     cmakeFlags = [
       "-DCMAKE_BUILD_TYPE=Production"
@@ -56,7 +75,7 @@ in
       "-DENABLE_ASAN=0"
       "-DENABLE_UBSAN=0"
       "-DENABLE_TSAN=0"
-      "-DENABLE_ASSERTION=0"
+      "-DENABLE_ASSERTIONS=0"
       "-DENABLE_COMP_INC_TRACK=0"
       "-DENABLE_DEBUG_SYMBOLS=0"
       "-DENABLE_DUMPING=0"
@@ -66,8 +85,8 @@ in
       "-DENABLE_TRACING=0"
       "-DENABLE_UNIT_TESTING=0"
       "-DENABLE_VALGRIND=0"
-      "-DENABLE_SHARED=1"
-      "-DENABLE_STATIC_BINARY=0"
+      "-DENABLE_SHARED=0"
+      "-DENABLE_STATIC_BINARY=1"
 
       "-DENABLE_BEST=0"
       "-DENABLE_COVERAGE=0"
@@ -78,7 +97,7 @@ in
       "-DUSE_CADICAL=1"
       "-DUSE_CLN=1"
       "-DUSE_CRYPTOMINISAT=1"
-      "-DUSE_GPLK=0"
+      "-DUSE_GLPK=0"
       "-DUSE_KISSAT=0"
       "-DUSE_READLINE=0"
 
