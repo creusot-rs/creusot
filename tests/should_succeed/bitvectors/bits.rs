20a7901682c4a30f72a9a6c64ee25849976b8735
Maël Coulmance <mcoulmance@gmail.com>
2026-01-20 15:52:01 +0100

    more tests

--- tests/should_succeed/bitvectors/bits.rs
+++ tests/should_succeed/bitvectors/bits.rs
@@ -1,7 +1,7 @@
 extern crate creusot_std;
 use creusot_std::prelude::*;
 
-const USIZE_BITS: usize = std::mem::size_of::<usize>() * 8; 
+pub const USIZE_BITS: usize = std::mem::size_of::<usize>() * 8;
 
 #[ensures(result == ffs_logic(val))]
 pub fn ffs(val: usize) -> u32 {
@@ -22,8 +22,36 @@ pub fn empty(val: usize) -> usize {
     let rq_ffs = ffs(val);
     if rq_ffs == 0 {
         0
-    } else { 
+    } else {
         let rq = (rq_ffs - 1) as u8;
         val & !(1 << rq)
     }
+}
+
+#[logic]
+pub fn has_bit(bitvec: usize, bit: u8) -> bool {
+    bitvec & (1usize << bit) != 0usize
+}
+
+#[bitwise_proof]
+#[requires(bit@ < USIZE_BITS@)]
+#[ensures(has_bit(result, bit))]
+#[ensures(forall<b: u8> has_bit(bitvec, b) ==> has_bit(result, b))]
+pub fn set_bit(bitvec: usize, bit: u8) -> usize {
+    bitvec | (1 << bit)
+}
+
+#[bitwise_proof]
+#[requires(bit@ < USIZE_BITS@)]
+#[ensures(!has_bit(result, bit))]
+#[ensures(forall<b: u8> b != bit && has_bit(bitvec, b) ==> has_bit(result, b))]
+pub fn unset_bit(bitvec: usize, bit: u8) -> usize {
+    bitvec & !(1 << bit)
+}
+
+#[bitwise_proof]
+#[ensures(forall<b: u8> has_bit(lhs, b) ==> has_bit(result, b))]
+#[ensures(forall<b: u8> has_bit(rhs, b) ==> has_bit(result, b))]
+pub fn merge_bvs(lhs: usize, rhs: usize) -> usize {
+    lhs | rhs
 }
