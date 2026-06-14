be86af9c1c2df952f2b30bbaf9f235582a2faf17
Diane Golfouse <git@dgolfouse.net>
2026-06-14 17:31:02 +0200

    feat(creusot-std): implement `such_that_decide`

--- creusot-std/src/logic.rs
+++ creusot-std/src/logic.rs
@@ -42,6 +42,23 @@ pub fn such_that<T>(p: crate::logic::Mapping<T, bool>) -> T {
     dead
 }
 
+/// Same as [`such_that`], but returns `None` if no witness exists.
+#[logic]
+#[ensures(match result {
+    None => forall<x: T> !p[x],
+    Some(x) => p[x],
+})]
+pub fn such_that_decide<T>(p: crate::logic::Mapping<T, bool>) -> Option<T> {
+    pearlite! {
+        if (forall<x: T> !p[x]) {
+            None
+        } else {
+            let x = such_that(p);
+            Some(x)
+        }
+    }
+}
+
 /// Indicates unreachable code in logic.
 ///
 /// This function indicates a logical branch that should be impossible to reach.
