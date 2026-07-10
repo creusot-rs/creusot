creusot-rs/creusot

commit df21379f97cf3909b9cd4f89b6baae558d03e429

Partially fix translation of non-trivial format strings

creusot/src/backend/term.rs
@@ -543,6 +543,7 @@ impl<'tcx, N: Namer<'tcx>> Lower<'_, 'tcx, N> {
             }
             Literal::ZST => Exp::unit(),
             Literal::String(ref string) => Constant::String(string.clone()).into(),
+            Literal::Bytes(ref bytes) => todo!(),
         }
     }
 }

commit eb52c4f130b506d17d26c17d7e8d9f67d298f17a

Translate literal bytes to Coma
Creusot failed to analyze Rust programs using literal bytes as they were
not translated in the generated Coma files. Introduce a new Coma
function `of_seq` in `Slice` modules to enable crafting a pure WhyML
term with the contents of the literal bytes.

Fixes: https://github.com/creusot-rs/creusot/issues/2161
Fixes: df21379f97cf ("Partially fix translation of non-trivial format
strings")

creusot/src/backend/term.rs
@@ -27,7 +27,7 @@ use rustc_middle::{
     ty::{self, Ty, TyCtxt, TyKind},
 };
 use rustc_span::Span;
-use rustc_type_ir::IntTy;
+use rustc_type_ir::{IntTy, UintTy};
 use why3::{
     Ident, Name,
     declaration::Condition as WCondition,
@@ -575,7 +575,22 @@ impl<'tcx, N: Namer<'tcx>> Lower<'_, 'tcx, N> {
                 Exp::unit()
             }
             Literal::String(ref string) => Constant::String(string.clone()).into(),
-            Literal::Bytes(ref _bytes) => todo!(),
+            Literal::Bytes(ref bytes) => {
+                let seq_exp = if bytes.is_empty() {
+                    Exp::qvar(name::seq_empty())
+                } else {
+                    let u8_ty = Type::qconstructor(
+                        self.names.in_pre(uty_to_prelude(self.tcx(), UintTy::U8), "t"),
+                    );
+                    let byte_exps: Box<[Exp]> = bytes
+                        .iter()
+                        .map(|&b| Constant::Uint(b as u128, Some(u8_ty.clone())).into())
+                        .collect();
+                    Exp::qvar(name::seq_create())
+                        .app([Exp::int(bytes.len() as i128), Exp::FunLiteral(byte_exps)])
+                };
+                Exp::qvar(self.names.in_pre(PreMod::Slice, "of_seq")).app([seq_exp])
+            }
         }
     }
 }

creusot/src/naming.rs
@@ -364,5 +364,6 @@ pub mod name {
         seq_get => ["Seq", "get"],
         seq_length => ["Seq", "length"],
         seq_create => ["Seq", "create"],
+        seq_empty => ["Seq", "empty"],
     }
 }

prelude-generator/slice.in.coma
@@ -14,6 +14,9 @@ module Slice$bits_count$
 
   function view (s : slice 'a) : seq 'a = s.elts
 
+  function of_seq (s : seq 'a) : slice 'a
+  axiom of_seq_elts : forall s : seq 'a [(of_seq s).elts] . UInt$bits_count$.in_bounds (Seq.length s) -> (of_seq s).elts = s
+
   function length (s : slice 'a) : UInt$bits_count$.t
   axiom length_spec : forall s : slice 'a . (UInt$bits_count$.t'int (length s)) = Seq.length s.elts
 
@@ -54,6 +57,9 @@ module Slice$bits_count$BW
   type array 'a = slice 'a
 
   function view (s : slice 'a) : seq 'a = s.elts
+
+  function of_seq (s : seq 'a) : slice 'a
+  axiom of_seq_elts : forall s : seq 'a [(of_seq s).elts] . UInt$bits_count$BW.in_bounds (Seq.length s) -> (of_seq s).elts = s
 
   function length (s : slice 'a) : UInt$bits_count$BW.t
   axiom length_spec : forall s : slice 'a . (UInt$bits_count$BW.t'int (length s)) = Seq.length s.elts

tests/creusot-std/creusot-std/why3session.xml
@@ -4575,7 +4575,7 @@
 </theory>
 <theory name="M_std__array__extern_spec_T_constN_array_T_as_mut_slice_body" proved="true">
  <goal name="vc_extern_spec_T__constN__array_T_as_mut_slice_body_T" proved="true">
- <proof prover="3"><result status="valid" time="0.023167" steps="316"/></proof>
+ <proof prover="3"><result status="valid" time="0.023167" steps="308"/></proof>
  </goal>
 </theory>
 <theory name="M_std__array__extern_spec_T_constN_array_T_as_slice_body" proved="true">
@@ -4645,7 +4645,7 @@
 </theory>
 <theory name="M_std__array__impl_Resolve_for_array_T_n__resolve_coherence__refines" proved="true">
  <goal name="refines" proved="true">
- <proof prover="3" timelimit="5"><result status="valid" time="0.014058" steps="0"/></proof>
+ <proof prover="3"><result status="valid" time="0.014058" steps="0"/></proof>
  </goal>
 </theory>
 <theory name="M_std__bool__extern_spec_bool_then_body" proved="true">
@@ -8734,7 +8734,8 @@
 </theory>
 <theory name="M_std__option__extern_spec_T_Option_T_as_mut_slice_body" proved="true">
  <goal name="vc_extern_spec_T_Option_T_as_mut_slice_body_T" proved="true">
- <proof prover="0"><result status="valid" time="0.012549" steps="36032"/></proof>
+ <proof prover="0"><result status="valid" time="0.012549" steps="45165"/></proof>
+ <proof prover="3"><result status="valid" time="0.079124" steps="1075"/></proof>
  </goal>
 </theory>
 <theory name="M_std__option__extern_spec_T_Option_T_as_ref_body" proved="true">
@@ -8744,7 +8745,7 @@
 </theory>
 <theory name="M_std__option__extern_spec_T_Option_T_as_slice_body" proved="true">
  <goal name="vc_extern_spec_T_Option_T_as_slice_body_T" proved="true">
- <proof prover="3" timelimit="5"><result status="valid" time="0.016306" steps="117"/></proof>
+ <proof prover="3"><result status="valid" time="0.016306" steps="117"/></proof>
  </goal>
 </theory>
 <theory name="M_std__option__extern_spec_T_Option_T_expect_body" proved="true">
@@ -9093,7 +9094,7 @@
 </theory>
 <theory name="M_std__ptr__impl_Perm_ptr_slice_T__index" proved="true">
  <goal name="vc_index_T" proved="true">
- <proof prover="3"><result status="valid" time="0.116873" steps="538"/></proof>
+ <proof prover="3"><result status="valid" time="0.116873" steps="537"/></proof>
  </goal>
 </theory>
 <theory name="M_std__ptr__impl_Perm_ptr_slice_T__index_mut" proved="true">
@@ -9117,10 +9118,11 @@
    <proof prover="3"><result status="valid" time="0.070157" steps="129"/></proof>
    </goal>
    <goal name="vc_index_mut_T.0.5" expl="assertion" proved="true">
-   <proof prover="0" timelimit="0.2"><result status="valid" time="0.042681" steps="75813"/></proof>
+   <proof prover="0" timelimit="0.2"><result status="valid" time="0.042681" steps="78141"/></proof>
+   <proof prover="3"><result status="valid" time="0.020691" steps="107"/></proof>
    </goal>
    <goal name="vc_index_mut_T.0.6" expl="index_mut_T ensures" proved="true">
-   <proof prover="0" timelimit="0.2"><result status="valid" time="0.013610" steps="89080"/></proof>
+   <proof prover="0" timelimit="0.2"><result status="valid" time="0.013610" steps="90029"/></proof>
    </goal>
    <goal name="vc_index_mut_T.0.7" expl="type invariant" proved="true">
    <proof prover="3"><result status="valid" time="0.040985" steps="40"/></proof>
@@ -9304,7 +9306,7 @@
 </theory>
 <theory name="M_std__slice__impl_DoubleEndedIteratorSpec_for_IterMut_T__size_hint_back_spec" proved="true">
  <goal name="vc_size_hint_back_spec_IterMut_T" proved="true">
- <proof prover="3"><result status="valid" time="0.025668" steps="226"/></proof>
+ <proof prover="3"><result status="valid" time="0.025668" steps="230"/></proof>
  </goal>
 </theory>
 <theory name="M_std__slice__impl_DoubleEndedIteratorSpec_for_IterMut_T__size_hint_back_spec__refines" proved="true">
@@ -9334,7 +9336,7 @@
 </theory>
 <theory name="M_std__slice__impl_DoubleEndedIteratorSpec_for_Iter_T__size_hint_back_spec" proved="true">
  <goal name="vc_size_hint_back_spec_Iter_T" proved="true">
- <proof prover="3"><result status="valid" time="0.038482" steps="185"/></proof>
+ <proof prover="3"><result status="valid" time="0.038482" steps="186"/></proof>
  </goal>
 </theory>
 <theory name="M_std__slice__impl_DoubleEndedIteratorSpec_for_Iter_T__size_hint_back_spec__refines" proved="true">
@@ -9369,7 +9371,8 @@
 </theory>
 <theory name="M_std__slice__impl_IteratorSpec_for_IterMut_T__produces_refl__refines" proved="true">
  <goal name="refines" proved="true">
- <proof prover="2" timelimit="1"><result status="valid" time="0.018707" steps="2072"/></proof>
+ <proof prover="2" timelimit="1"><result status="valid" time="0.018707" steps="2290"/></proof>
+ <proof prover="3"><result status="valid" time="0.012028" steps="0"/></proof>
  </goal>
 </theory>
 <theory name="M_std__slice__impl_IteratorSpec_for_IterMut_T__produces_trans" proved="true">
@@ -9379,7 +9382,7 @@
 </theory>
 <theory name="M_std__slice__impl_IteratorSpec_for_IterMut_T__produces_trans__refines" proved="true">
  <goal name="refines" proved="true">
- <proof prover="3" timelimit="5"><result status="valid" time="0.015003" steps="2"/></proof>
+ <proof prover="3"><result status="valid" time="0.015003" steps="2"/></proof>
  </goal>
 </theory>
 <theory name="M_std__slice__impl_IteratorSpec_for_Iter_T__produces_refl" proved="true">
@@ -9389,7 +9392,8 @@
 </theory>
 <theory name="M_std__slice__impl_IteratorSpec_for_Iter_T__produces_refl__refines" proved="true">
  <goal name="refines" proved="true">
- <proof prover="2" timelimit="1"><result status="valid" time="0.018380" steps="1353"/></proof>
+ <proof prover="2" timelimit="1"><result status="valid" time="0.018380" steps="1501"/></proof>
+ <proof prover="3"><result status="valid" time="0.012626" steps="0"/></proof>
  </goal>
 </theory>
 <theory name="M_std__slice__impl_IteratorSpec_for_Iter_T__produces_trans" proved="true">
@@ -9399,7 +9403,7 @@
 </theory>
 <theory name="M_std__slice__impl_IteratorSpec_for_Iter_T__produces_trans__refines" proved="true">
  <goal name="refines" proved="true">
- <proof prover="3" timelimit="5"><result status="valid" time="0.014739" steps="2"/></proof>
+ <proof prover="3"><result status="valid" time="0.014739" steps="2"/></proof>
  </goal>
 </theory>
 <theory name="M_std__slice__impl_Resolve_for_IterMut_T__resolve_coherence__refines" proved="true">
@@ -9409,7 +9413,7 @@
 </theory>
 <theory name="M_std__slice__impl_Resolve_for_slice_T__resolve_coherence__refines" proved="true">
  <goal name="refines" proved="true">
- <proof prover="3" timelimit="1"><result status="valid" time="0.028848" steps="0"/></proof>
+ <proof prover="3"><result status="valid" time="0.028848" steps="0"/></proof>
  </goal>
 </theory>
 <theory name="M_std__slice__impl_SliceExt_for_slice_T__as_mut_ptr_perm" proved="true">

tests/should_fail/bug/878/why3session.xml
@@ -14,9 +14,9 @@
   <goal name="vc_test.0">
   <transf name="split_vc" >
    <goal name="vc_test.0.0">
-   <proof prover="0"><result status="timeout" time="1.000000" steps="8156876"/></proof>
-   <proof prover="1"><result status="unknown" time="0.047424" steps="4588"/></proof>
-   <proof prover="2"><result status="unknown" time="0.071711" steps="10210"/></proof>
+   <proof prover="0"><result status="timeout" time="1.000000" steps="10186700"/></proof>
+   <proof prover="1"><result status="unknown" time="0.047424" steps="4865"/></proof>
+   <proof prover="2"><result status="unknown" time="0.071711" steps="10353"/></proof>
    <proof prover="3"><result status="unknown" time="0.026794" steps="39"/></proof>
    </goal>
   </transf>
@@ -33,10 +33,10 @@
   <goal name="vc_test2.1">
   <transf name="split_vc" >
    <goal name="vc_test2.1.0">
-   <proof prover="0"><result status="timeout" time="1.000000" steps="16673739"/></proof>
-   <proof prover="1"><result status="unknown" time="0.049869" steps="4777"/></proof>
-   <proof prover="2"><result status="unknown" time="0.079446" steps="9701"/></proof>
-   <proof prover="3"><result status="unknown" time="0.200839" steps="1107"/></proof>
+   <proof prover="0"><result status="timeout" time="1.000000" steps="46641633"/></proof>
+   <proof prover="1"><result status="unknown" time="0.049869" steps="5054"/></proof>
+   <proof prover="2"><result status="unknown" time="0.079446" steps="9844"/></proof>
+   <proof prover="3"><result status="unknown" time="0.200839" steps="1097"/></proof>
    </goal>
   </transf>
   </goal>

tests/should_fail/zst/why3session.xml
@@ -12,31 +12,31 @@
  <goal name="vc_zst_pointers_may_be_equal">
  <transf name="split_vc" >
   <goal name="vc_zst_pointers_may_be_equal.0" expl="index_mut requires" proved="true">
-  <proof prover="3"><result status="valid" time="0.051618" steps="3173"/></proof>
+  <proof prover="3"><result status="valid" time="0.051618" steps="3316"/></proof>
   </goal>
   <goal name="vc_zst_pointers_may_be_equal.1" expl="index requires" proved="true">
-  <proof prover="3"><result status="valid" time="0.054120" steps="3641"/></proof>
+  <proof prover="3"><result status="valid" time="0.054120" steps="3784"/></proof>
   </goal>
   <goal name="vc_zst_pointers_may_be_equal.2" expl="into_inner &#39;self&#39; type invariant" proved="true">
-  <proof prover="3"><result status="valid" time="0.046863" steps="3421"/></proof>
+  <proof prover="3"><result status="valid" time="0.046863" steps="3561"/></proof>
   </goal>
   <goal name="vc_zst_pointers_may_be_equal.3" expl="into_inner &#39;self&#39; type invariant" proved="true">
-  <proof prover="3"><result status="valid" time="0.051594" steps="3419"/></proof>
+  <proof prover="3"><result status="valid" time="0.051594" steps="3559"/></proof>
   </goal>
   <goal name="vc_zst_pointers_may_be_equal.4" expl="disjoint_lemma_ptr_unit requires" proved="true">
-  <proof prover="3"><result status="valid" time="0.048342" steps="3455"/></proof>
+  <proof prover="3"><result status="valid" time="0.048342" steps="3595"/></proof>
   </goal>
   <goal name="vc_zst_pointers_may_be_equal.5" expl="assertion">
-  <proof prover="0"><result status="unknown" time="0.100708" steps="8812"/></proof>
-  <proof prover="1"><result status="timeout" time="1.000000" steps="30247"/></proof>
-  <proof prover="2"><result status="timeout" time="1.000000" steps="41520141"/></proof>
-  <proof prover="3"><result status="unknown" time="0.054909" steps="13396"/></proof>
+  <proof prover="0"><result status="unknown" time="0.100708" steps="9089"/></proof>
+  <proof prover="1"><result status="timeout" time="1.000000" steps="27218"/></proof>
+  <proof prover="2"><result status="timeout" time="1.000000" steps="38558142"/></proof>
+  <proof prover="3"><result status="unknown" time="0.054909" steps="13539"/></proof>
   </goal>
   <goal name="vc_zst_pointers_may_be_equal.6" expl="type invariant" proved="true">
-  <proof prover="3"><result status="valid" time="0.050269" steps="3457"/></proof>
+  <proof prover="3"><result status="valid" time="0.050269" steps="3597"/></proof>
   </goal>
   <goal name="vc_zst_pointers_may_be_equal.7" expl="type invariant" proved="true">
-  <proof prover="3"><result status="valid" time="0.052906" steps="3420"/></proof>
+  <proof prover="3"><result status="valid" time="0.052906" steps="3560"/></proof>
   </goal>
  </transf>
  </goal>

tests/should_succeed/lang/literals.coma
@@ -1,3 +1,333 @@
+module M_TEST_BYTES
+  use creusot.slice.Slice64
+  use creusot.int.UInt8
+  use seq.Seq
+  use creusot.int.UInt64
+  use creusot.prelude.Any
+  
+  predicate inv_Seq_u8 [@inline:trivial] (_1: Seq.seq UInt8.t) = true
+  
+  meta "rewrite_def" predicate inv_Seq_u8
+  
+  predicate invariant_array_u8_10 (self: Slice64.array UInt8.t) =
+    inv_Seq_u8 (Slice64.view self) /\ Seq.length (Slice64.view self) = UInt64.t'int (10: UInt64.t)
+  
+  predicate inv_array_u8_10 [@inline:trivial] (_1: Slice64.array UInt8.t) = invariant_array_u8_10 _1
+  
+  meta "rewrite_def" predicate inv_array_u8_10
+  
+  predicate invariant_ref_array_u8_10 [@inline:trivial] (self: Slice64.array UInt8.t) = inv_array_u8_10 self
+  
+  meta "rewrite_def" predicate invariant_ref_array_u8_10
+  
+  predicate inv_ref_array_u8_10 [@inline:trivial] (_1: Slice64.array UInt8.t) = invariant_ref_array_u8_10 _1
+  
+  meta "rewrite_def" predicate inv_ref_array_u8_10
+  
+  constant const_TEST_BYTES : Slice64.array UInt8.t
+  
+  axiom const_TEST_BYTES_spec: inv_ref_array_u8_10 const_TEST_BYTES
+  
+  meta "compute_max_steps" 1000000
+  
+  meta "select_lsinst" "all"
+  
+  let const_TEST_BYTES_def (return (x: Slice64.array UInt8.t)) = (! bb0
+    [ bb0 = s0
+      [ s0 =
+        [ &_ret <- Slice64.of_seq (Seq.create 10 [|(99: UInt8.t);(111: UInt8.t);(110: UInt8.t);(115: UInt8.t);(116: UInt8.t);(32: UInt8.t);(116: UInt8.t);(101: UInt8.t);(115: UInt8.t);(116: UInt8.t)|]) ]
+        s1
+      | s1 = return {_ret} ] ] [ & _ret: Slice64.array UInt8.t = Any.any_l () ])
+    [ return (result: Slice64.array UInt8.t) -> -{const_TEST_BYTES = result}-
+      {[@stop_split] [@expl:TEST_BYTES result type invariant] inv_ref_array_u8_10 result}
+      (! return {result}) ]
+end
+module M_bytes_as_array
+  use creusot.slice.Slice64
+  use creusot.int.UInt8
+  use creusot.int.UInt64
+  use creusot.prelude.Any
+  
+  meta "compute_max_steps" 1000000
+  
+  meta "select_lsinst" "all"
+  
+  let bytes_as_array (return (x: UInt8.t)) = (! bb0
+    [ bb0 = s0
+      [ s0 = any
+        [ any_ (__arr_temp: Slice64.array UInt8.t) -> (! -{Seq.get __arr_temp.Slice64.elts 0 = (65: UInt8.t)
+          /\ Seq.get __arr_temp.Slice64.elts 1 = (32: UInt8.t)
+          /\ Seq.get __arr_temp.Slice64.elts 2 = (116: UInt8.t)
+          /\ Seq.get __arr_temp.Slice64.elts 3 = (101: UInt8.t)
+          /\ Seq.get __arr_temp.Slice64.elts 4 = (115: UInt8.t)
+          /\ Seq.get __arr_temp.Slice64.elts 5 = (116: UInt8.t) /\ Seq.length __arr_temp.Slice64.elts = 6}-
+          [ &my_bytes <- __arr_temp ] s1) ]
+      | s1 = [ &_3 <- (3: UInt64.t) ] s2
+      | s2 = [ &_4 <- UInt64.lt _3 (6: UInt64.t) ] s3
+      | s3 = {[@expl:index in bounds] _4} s4
+      | s4 = Slice64.get <UInt8.t> {my_bytes} {_3} (fun (r: UInt8.t) -> [ &_ret <- r ] s5)
+      | s5 = return {_ret} ] ]
+    [ & _ret: UInt8.t = Any.any_l ()
+    | & my_bytes: Slice64.array UInt8.t = Any.any_l ()
+    | & _3: UInt64.t = Any.any_l ()
+    | & _4: bool = Any.any_l () ])
+    [ return (result: UInt8.t) -> {[@stop_split] [@expl:bytes_as_array ensures] result = (101: UInt8.t)}
+      (! return {result}) ]
+end
+module M_bytes_as_array_slice
+  use creusot.slice.Slice64
+  use creusot.int.UInt8
+  use creusot.prelude.Any
+  use creusot.int.UInt64
+  
+  meta "compute_max_steps" 1000000
+  
+  meta "select_lsinst" "all"
+  
+  let bytes_as_array_slice (return (x: UInt8.t)) = (! bb0
+    [ bb0 = s0
+      [ s0 = bb0'0
+        [ bb0'0 = s0'0
+          [ s0'0 = any
+            [ any_ (__arr_temp: Slice64.array UInt8.t) -> (! -{Seq.get __arr_temp.Slice64.elts 0 = (65: UInt8.t)
+              /\ Seq.get __arr_temp.Slice64.elts 1 = (32: UInt8.t)
+              /\ Seq.get __arr_temp.Slice64.elts 2 = (116: UInt8.t)
+              /\ Seq.get __arr_temp.Slice64.elts 3 = (101: UInt8.t)
+              /\ Seq.get __arr_temp.Slice64.elts 4 = (115: UInt8.t)
+              /\ Seq.get __arr_temp.Slice64.elts 5 = (116: UInt8.t) /\ Seq.length __arr_temp.Slice64.elts = 6}-
+              [ &_1 <- __arr_temp ] s1'0) ]
+          | s1'0 = [ &_ret'0 <- _1 ] s2'0
+          | s2'0 = _const_ret {_ret'0} ] ]
+        [ & _ret'0: Slice64.array UInt8.t = Any.any_l () | & _1: Slice64.array UInt8.t = Any.any_l () ]
+        [ _const_ret (_const: Slice64.array UInt8.t) -> [ &_7 <- _const ] s1 ]
+      | s1 = [ &_3 <- _7 ] s2
+      | s2 = [ &my_bytes <- _3 ] s3
+      | s3 = [ &_5 <- (3: UInt64.t) ] s4
+      | s4 = [ &_6 <- UInt64.lt _5 (6: UInt64.t) ] s5
+      | s5 = {[@expl:index in bounds] _6} s6
+      | s6 = Slice64.get <UInt8.t> {my_bytes} {_5} (fun (r: UInt8.t) -> [ &_ret <- r ] s7)
+      | s7 = return {_ret} ] ]
+    [ & _ret: UInt8.t = Any.any_l ()
+    | & my_bytes: Slice64.array UInt8.t = Any.any_l ()
+    | & _3: Slice64.array UInt8.t = Any.any_l ()
+    | & _5: UInt64.t = Any.any_l ()
+    | & _6: bool = Any.any_l ()
+    | & _7: Slice64.array UInt8.t = Any.any_l () ])
+    [ return (result: UInt8.t) -> {[@stop_split] [@expl:bytes_as_array_slice ensures] result = (101: UInt8.t)}
+      (! return {result}) ]
+end
+module M_bytes_literal
+  use creusot.int.UInt8
+  use creusot.slice.Slice64
+  use creusot.prelude.Any
+  use seq.Seq
+  use creusot.int.UInt64
+  
+  predicate inv_Seq_u8 [@inline:trivial] (_1: Seq.seq UInt8.t) = true
+  
+  meta "rewrite_def" predicate inv_Seq_u8
+  
+  predicate invariant_array_u8_4 (self: Slice64.array UInt8.t) =
+    inv_Seq_u8 (Slice64.view self) /\ Seq.length (Slice64.view self) = UInt64.t'int (4: UInt64.t)
+  
+  predicate inv_array_u8_4 [@inline:trivial] (_1: Slice64.array UInt8.t) = invariant_array_u8_4 _1
+  
+  meta "rewrite_def" predicate inv_array_u8_4
+  
+  predicate invariant_ref_array_u8_4 [@inline:trivial] (self: Slice64.array UInt8.t) = inv_array_u8_4 self
+  
+  meta "rewrite_def" predicate invariant_ref_array_u8_4
+  
+  predicate inv_ref_array_u8_4 [@inline:trivial] (_1: Slice64.array UInt8.t) = invariant_ref_array_u8_4 _1
+  
+  meta "rewrite_def" predicate inv_ref_array_u8_4
+  
+  meta "compute_max_steps" 1000000
+  
+  meta "select_lsinst" "all"
+  
+  let bytes_literal (return (x: Slice64.array UInt8.t)) = (! bb0
+    [ bb0 = s0
+      [ s0 = [ &_ret <- Slice64.of_seq (Seq.create 4 [|(116: UInt8.t);(101: UInt8.t);(115: UInt8.t);(116: UInt8.t)|]) ]
+        s1
+      | s1 = return {_ret} ] ] [ & _ret: Slice64.array UInt8.t = Any.any_l () ])
+    [ return (result: Slice64.array UInt8.t) ->
+    {[@stop_split] [@expl:bytes_literal ensures] ([@stop_split] [@expl:bytes_literal result type invariant] inv_ref_array_u8_4 result)
+      /\ ([@stop_split] [@expl:bytes_literal ensures #0] Seq.length (Slice64.view result) = 4)
+      /\ ([@stop_split] [@expl:bytes_literal ensures #1] Slice64.view result
+      = Seq.create 4 [|(116: UInt8.t);(101: UInt8.t);(115: UInt8.t);(116: UInt8.t)|])}
+      (! return {result}) ]
+end
+module M_bytes_literal_bw
+  use creusot.int.UInt8BW
+  use creusot.slice.Slice64BW
+  use creusot.int.UInt64BW
+  use creusot.prelude.Any
+  
+  meta "compute_max_steps" 1000000
+  
+  meta "select_lsinst" "all"
+  
+  let bytes_literal_bw (return (x: UInt8BW.t)) = (! bb0
+    [ bb0 = s0
+      [ s0 =
+        [ &_3 <- Slice64BW.of_seq (Seq.create 6 [|(65: UInt8BW.t);(32: UInt8BW.t);(116: UInt8BW.t);(101: UInt8BW.t);(115: UInt8BW.t);(116: UInt8BW.t)|]) ]
+        s1
+      | s1 = [ &my_bytes <- _3 ] s2
+      | s2 = [ &_4 <- (3: UInt64BW.t) ] s3
+      | s3 = [ &_5 <- UInt64BW.lt _4 (6: UInt64BW.t) ] s4
+      | s4 = {[@expl:index in bounds] _5} s5
+      | s5 = Slice64BW.get <UInt8BW.t> {my_bytes} {_4} (fun (r: UInt8BW.t) -> [ &_ret <- r ] s6)
+      | s6 = return {_ret} ] ]
+    [ & _ret: UInt8BW.t = Any.any_l ()
+    | & my_bytes: Slice64BW.array UInt8BW.t = Any.any_l ()
+    | & _3: Slice64BW.array UInt8BW.t = Any.any_l ()
+    | & _4: UInt64BW.t = Any.any_l ()
+    | & _5: bool = Any.any_l () ])
+    [ return (result: UInt8BW.t) -> {[@stop_split] [@expl:bytes_literal_bw ensures] result = (101: UInt8BW.t)}
+      (! return {result}) ]
+end
+module M_bytes_literal_empty
+  use creusot.slice.Slice64
+  use creusot.int.UInt8
+  use creusot.prelude.Any
+  use seq.Seq
+  use creusot.int.UInt64
+  
+  predicate inv_Seq_u8 [@inline:trivial] (_1: Seq.seq UInt8.t) = true
+  
+  meta "rewrite_def" predicate inv_Seq_u8
+  
+  predicate invariant_array_u8_0 (self: Slice64.array UInt8.t) =
+    inv_Seq_u8 (Slice64.view self) /\ Seq.length (Slice64.view self) = UInt64.t'int (0: UInt64.t)
+  
+  predicate inv_array_u8_0 [@inline:trivial] (_1: Slice64.array UInt8.t) = invariant_array_u8_0 _1
+  
+  meta "rewrite_def" predicate inv_array_u8_0
+  
+  predicate invariant_ref_array_u8_0 [@inline:trivial] (self: Slice64.array UInt8.t) = inv_array_u8_0 self
+  
+  meta "rewrite_def" predicate invariant_ref_array_u8_0
+  
+  predicate inv_ref_array_u8_0 [@inline:trivial] (_1: Slice64.array UInt8.t) = invariant_ref_array_u8_0 _1
+  
+  meta "rewrite_def" predicate inv_ref_array_u8_0
+  
+  meta "compute_max_steps" 1000000
+  
+  meta "select_lsinst" "all"
+  
+  let bytes_literal_empty (return (x: Slice64.array UInt8.t)) = (! bb0
+    [ bb0 = s0 [ s0 = [ &_ret <- Slice64.of_seq Seq.empty ] s1 | s1 = return {_ret} ] ]
+    [ & _ret: Slice64.array UInt8.t = Any.any_l () ])
+    [ return (result: Slice64.array UInt8.t) ->
+    {[@stop_split] [@expl:bytes_literal_empty ensures] ([@stop_split] [@expl:bytes_literal_empty result type invariant] inv_ref_array_u8_0 result)
+      /\ ([@stop_split] [@expl:bytes_literal_empty ensures] Seq.length (Slice64.view result) = 0)}
+      (! return {result}) ]
+end
+module M_bytes_literal_in_body
+  use creusot.int.UInt8
+  use creusot.slice.Slice64
+  use creusot.int.UInt64
+  use seq.Seq
+  use creusot.prelude.Any
+  
+  let len_u8 (self_: Slice64.slice UInt8.t) (return (x: UInt64.t)) = any
+    [ return (result: UInt64.t) -> {[@stop_split] [@expl:len ensures] Seq.length (Slice64.view self_)
+      = UInt64.t'int result}
+      (! return {result}) ]
+  
+  meta "compute_max_steps" 1000000
+  
+  meta "select_lsinst" "all"
+  
+  let bytes_literal_in_body (return (x: UInt64.t)) = (! bb0
+    [ bb0 = s0
+      [ s0 = [ &_3 <- Slice64.of_seq (Seq.create 4 [|(116: UInt8.t);(101: UInt8.t);(115: UInt8.t);(116: UInt8.t)|]) ] s1
+      | s1 = [ &msg <- _3 ] s2
+      | s2 = len_u8 {msg} (fun (_x: UInt64.t) -> [ &_ret <- _x ] s3)
+      | s3 = return {_ret} ] ]
+    [ & _ret: UInt64.t = Any.any_l ()
+    | & msg: Slice64.array UInt8.t = Any.any_l ()
+    | & _3: Slice64.array UInt8.t = Any.any_l () ])
+    [ return (result: UInt64.t) -> {[@stop_split] [@expl:bytes_literal_in_body ensures] result = (4: UInt64.t)}
+      (! return {result}) ]
+end
+module M_bytes_literal_in_const
+  use creusot.slice.Slice64
+  use creusot.int.UInt8
+  use seq.Seq
+  use creusot.int.UInt64
+  use creusot.prelude.Any
+  
+  predicate inv_Seq_u8 [@inline:trivial] (_1: Seq.seq UInt8.t) = true
+  
+  meta "rewrite_def" predicate inv_Seq_u8
+  
+  predicate invariant_array_u8_10 (self: Slice64.array UInt8.t) =
+    inv_Seq_u8 (Slice64.view self) /\ Seq.length (Slice64.view self) = UInt64.t'int (10: UInt64.t)
+  
+  predicate inv_array_u8_10 [@inline:trivial] (_1: Slice64.array UInt8.t) = invariant_array_u8_10 _1
+  
+  meta "rewrite_def" predicate inv_array_u8_10
+  
+  predicate invariant_ref_array_u8_10 [@inline:trivial] (self: Slice64.array UInt8.t) = inv_array_u8_10 self
+  
+  meta "rewrite_def" predicate invariant_ref_array_u8_10
+  
+  predicate inv_ref_array_u8_10 [@inline:trivial] (_1: Slice64.array UInt8.t) = invariant_ref_array_u8_10 _1
+  
+  meta "rewrite_def" predicate inv_ref_array_u8_10
+  
+  constant const_TEST_BYTES: Slice64.array UInt8.t = Slice64.of_seq (Seq.create 10 [|(99: UInt8.t);(111: UInt8.t);(110: UInt8.t);(115: UInt8.t);(116: UInt8.t);(32: UInt8.t);(116: UInt8.t);(101: UInt8.t);(115: UInt8.t);(116: UInt8.t)|])
+  
+  axiom const_TEST_BYTES_spec: inv_ref_array_u8_10 const_TEST_BYTES
+  
+  meta "compute_max_steps" 1000000
+  
+  meta "select_lsinst" "all"
+  
+  let bytes_literal_in_const (return (x: UInt8.t)) = (! bb0
+    [ bb0 = s0
+      [ s0 = [ &_2 <- const_TEST_BYTES ] s1
+      | s1 = [ &_3 <- (9: UInt64.t) ] s2
+      | s2 = [ &_4 <- UInt64.lt _3 (10: UInt64.t) ] s3
+      | s3 = {[@expl:index in bounds] _4} s4
+      | s4 = Slice64.get <UInt8.t> {_2} {_3} (fun (r: UInt8.t) -> [ &_ret <- r ] s5)
+      | s5 = return {_ret} ] ]
+    [ & _ret: UInt8.t = Any.any_l ()
+    | & _2: Slice64.array UInt8.t = Any.any_l ()
+    | & _3: UInt64.t = Any.any_l ()
+    | & _4: bool = Any.any_l () ])
+    [ return (result: UInt8.t) -> {[@stop_split] [@expl:bytes_literal_in_const ensures] result = (116: UInt8.t)}
+      (! return {result}) ]
+end
+module M_empty_array
+  use creusot.slice.Slice64
+  use creusot.int.UInt32
+  use creusot.int.UInt64
+  use seq.Seq
+  use creusot.prelude.Any
+  
+  let len_u32 (self_: Slice64.slice UInt32.t) (return (x: UInt64.t)) = any
+    [ return (result: UInt64.t) -> {[@stop_split] [@expl:len ensures] Seq.length (Slice64.view self_)
+      = UInt64.t'int result}
+      (! return {result}) ]
+  
+  meta "compute_max_steps" 1000000
+  
+  meta "select_lsinst" "all"
+  
+  let empty_array (return (x: UInt64.t)) = (! bb0
+    [ bb0 = s0
+      [ s0 = any
+        [ any_ (__arr_temp: Slice64.array UInt32.t) -> (! -{Seq.length __arr_temp.Slice64.elts = 0}-
+          [ &empty <- __arr_temp ] s1) ]
+      | s1 = len_u32 {empty} (fun (_x: UInt64.t) -> [ &_ret <- _x ] s2)
+      | s2 = return {_ret} ] ] [ & _ret: UInt64.t = Any.any_l () | & empty: Slice64.array UInt32.t = Any.any_l () ])
+    [ return (result: UInt64.t) -> {[@stop_split] [@expl:empty_array ensures] result = (0: UInt64.t)}
+      (! return {result}) ]
+end
 module M_float_operation
   use creusot.float.Float32
   use creusot.prelude.Any

tests/should_succeed/lang/literals.rs
@@ -1,7 +1,57 @@
 extern crate creusot_std;
+use creusot_std::prelude::*;
 
 pub fn float_operation() -> f32 {
     let x: f32 = 0.0;
 
     if x + 1.0005 == 2.0 { 3.0 - 1.0 } else { 0.0 }
+}
+
+#[ensures(result@.len() == 4)]
+#[ensures(result@ == seq![0x74u8, 0x65u8, 0x73u8, 0x74u8])]
+pub fn bytes_literal() -> &'static [u8; 4] {
+    b"test"
+}
+
+#[ensures(result@.len() == 0)]
+pub fn bytes_literal_empty() -> &'static [u8; 0] {
+    b""
+}
+
+#[ensures(result == 4_usize)]
+pub fn bytes_literal_in_body() -> usize {
+    let msg: &[u8; 4] = b"test";
+    msg.len()
+}
+
+const TEST_BYTES: &[u8; 10] = b"const test";
+
+#[ensures(result == 0x74_u8)] // 't'
+pub fn bytes_literal_in_const() -> u8 {
+    TEST_BYTES[9]
+}
+
+#[bitwise_proof]
+#[ensures(result == 0x65_u8)] // 'e'
+pub fn bytes_literal_bw() -> u8 {
+    let my_bytes: &[u8; 6] = b"A test";
+    my_bytes[3]
+}
+
+#[ensures(result == 0x65_u8)]
+pub fn bytes_as_array() -> u8 {
+    let my_bytes: [u8; 6] = [b'A', b' ', b't', b'e', b's', b't'];
+    my_bytes[3]
+}
+
+#[ensures(result == 0x65_u8)]
+pub fn bytes_as_array_slice() -> u8 {
+    let my_bytes: &[u8; 6] = &[b'A', b' ', b't', b'e', b's', b't'];
+    my_bytes[3]
+}
+
+#[ensures(result == 0_usize)]
+pub fn empty_array() -> usize {
+    let empty: [u32; 0] = [];
+    empty.len()
 }