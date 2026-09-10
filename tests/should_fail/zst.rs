9811e3b217c8dbf891131881af749e995a50cc9d
Diane Golfouse <git@dgolfouse.net>
2026-09-10 17:15:24 +0200

    refactor!(perms): use guarded borrows for `<&mut T>::from_mut`

--- tests/should_fail/zst.rs
+++ tests/should_fail/zst.rs
@@ -8,8 +8,9 @@ use creusot_std::{
 pub fn zst_pointers_may_be_equal() {
     let mut v0: Vec<()> = vec![()];
     let v1: Vec<()> = vec![()];
-    let (p1, own1) = Perm::from_mut(&mut v0[0]);
+    let (p1, mut own1) = Perm::from_mut(&mut v0[0]);
     let (p2, own2) = Perm::from_ref(&v1[0]);
+    ghost_let!(own1 = &mut *own1.borrow);
     ghost! { Perm::disjoint_lemma(own1.into_inner(), own2.into_inner()) };
     // This is actually false because p1 and p2 are both equal to NonNull::dangling()
     // The fix was to add a precondition to disjoint_lemma that the pointee size should be non-zero
